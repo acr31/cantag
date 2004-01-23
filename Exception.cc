85c61f3f79bb42282206aaf14cd57cd360634552
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2004-01-23 22:36:12 +0000

    Added missing include for Exception.hh
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@100 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- Exception.cc
+++ Exception.cc
@@ -2,10 +2,15 @@
  * $Header$
  *
  * $Log$
+ * Revision 1.3  2004/01/23 22:36:12  acr31
+ * Added missing include for Exception.hh
+ *
  * Revision 1.2  2004/01/21 11:55:07  acr31
  * added keywords for substitution
  *
  */
+#include "Exception.hh"
+
 std::ostream& operator<<(std::ostream& s, const Exception& z) {
   return z.Print(s);
 }
