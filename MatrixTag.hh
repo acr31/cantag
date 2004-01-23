e119ac924c0a0cbc4cf36f6bbe5fa6559abc95c2
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2004-01-23 22:36:39 +0000

    added a cyclic reading and writing scheme
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@101 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- MatrixTag.hh
+++ MatrixTag.hh
@@ -12,6 +12,9 @@
  * $Header$
  *
  * $Log$
+ * Revision 1.2  2004/01/23 22:36:39  acr31
+ * added a cyclic reading and writing scheme
+ *
  * Revision 1.1  2004/01/23 18:18:11  acr31
  * added Matrix Tag and a test program and the beginning of the CRC coder
  *
@@ -32,53 +35,71 @@
 template<class C>
 class MatrixTag : virtual Tag<Rectangle2D> {
 private:
+  int m_length;
   C m_coder;
   int m_size;
-  float *m_cells_bottom_left;
+  float m_cell_width;
+  float m_cell_width_2;
+  float *m_cells_corner;
 
 public:
   MatrixTag(int size) :
-    m_coder(2,size*size),
-    m_size(size+2) {
-    
-    // we draw an outer border of black cells
-    // ie (0,_) = black
-    //    (size+1,_) = black
-    //    (_,0) = black
-    //    (_,size+1) = black
-    //
-    // This leaves a space of size x size cells in the middle for the code
-    int arraysize = m_size*m_size;
-    m_cells_bottom_left = new float[arraysize];
-    
-    // our first cell starts at position 0,0 and is
-    // m_cells_bottom_left[0],m_cells_bottom_left[1]
+    m_length(size*size - (size*size % 2)),
+    m_coder(2,size*size - (size*size % 2)),
+    m_size(size),
+    m_cell_width(1/(float)(size+2)),
+    m_cell_width_2(0.5/(float)(size+2))
+  {
 
-    // the bottom right cell is at 1,0 and is
-    // m_cells_bottom_left[2*size-2],m_cells_bottom_left[2*size-1]
-    
-    // the top left cell is at 0,1 and is
-    // m_cells_bottom_left[2*size * (size-1)],m_cells_bottom_left[2*size * (size-1)+1]
+    /* we read the tag in triangles:
+     *
+     *    1  2  3  4  5  6  7 17       1  2  3  4  5  6 13
+     *   55  8  9 10 11 12 24 18      43  7  8  9 10 19 14
+     *   54 60 13 14 15 29 25 19      42 47 11 12 23 20 15
+     *   53 59 63 16 32 30 26 20      41 46 49    24 21 16
+     *   52 58 62 64 48 31 27 21      40 45 48 37 36 22 17
+     *   51 57 61 47 46 45 28 22      39 44 35 34 33 32 18
+     *   50 56 44 43 42 41 40 23      38 30 29 28 27 26 25
+     *   49 39 38 37 36 35 34 33
+     *
+     * This way the only possible ways of reading the code are rotations of each other
+     *
+     * If the tag has an odd number of elements then the middle
+     * element would be unread.  So we leave it leave it unused.  The
+     * other option is to read it 4 times at the top of each triangle.
+     *
+     */
+    m_cells_corner = new float[m_size*m_size];
+    int triangle_size = m_size*m_size / 2;
+    int position = 0;
+    for(int height = 0; height < m_size/2; height++) {
+      for(int i=height;i<m_size-1-height;i++) {
+	m_cells_corner[position] = (float)(i+1)/(m_size+2);
+	m_cells_corner[position+1] = (float)(height+1)/(m_size+2);
 
-    // the top right cell is at 1,1 and is
-    // m_cells_bottom_left[2*size * size-2],m_cells_bottom_left[2*size * size-1]
+	m_cells_corner[position+triangle_size] = (float)(m_size-height)/(m_size+2);
+	m_cells_corner[position+triangle_size+1] = (float)(i+1)/(m_size+2);
 
-    for(int i=0;i<m_size;i++) {
-      for(int j=0;j<m_size;j++) {
-	m_cells_bottom_left[i*2*m_size + 2*j] = (float)j/(float)m_size;
-	m_cells_bottom_left[i*2*m_size + 2*j+1] = (float)i/(float)m_size;
-	std::cout << "("<<(float)j/(float)m_size << "," << (float)i/(float)m_size <<") ";
+	m_cells_corner[position+triangle_size*2] = (float)(m_size-i)/(m_size+2);
+	m_cells_corner[position+triangle_size*2+1] = (float)(m_size-height)/(m_size+2);
+
+	m_cells_corner[position+triangle_size*3] = (float)(height+1)/(m_size+2);
+	m_cells_corner[position+triangle_size*3+1] = (float)(m_size-i)/(m_size+2);
+	position+=2;
       }
-      std::cout << std::endl;
     }
   }
 
   virtual ~MatrixTag() {
-    delete[] m_cells_bottom_left;
   }
+
   
-  virtual void Draw2D(Image* image, const Rectangle2D *l, unsigned long code, int black, int white) {
-    PROGRESS("Drawing tag (" << l->m_x0 << "," << l->m_y0 << ") (" << l->m_x1 << "," << l->m_y1 << ") (" << l->m_x2 << "," << l->m_y2 << ") (" << l->m_x3 << ","<< l->m_y3<<")");
+  virtual void Draw2D(Image* image, const Rectangle2D *l, unsigned long long code, int black, int white) {
+    PROGRESS("Drawing tag " <<
+	     "(" << l->m_x0 << "," << l->m_y0 << ") " <<
+	     "(" << l->m_x1 << "," << l->m_y1 << ") " <<
+	     "(" << l->m_x2 << "," << l->m_y2 << ") " <<
+	     "(" << l->m_x3 << "," << l->m_y3 << ")");
     DrawFilledQuadTangle(image,
 			 l->m_x0, l->m_y0,
 			 l->m_x1, l->m_y1,
@@ -92,23 +113,59 @@ public:
     float projX1, projY1;
     float projX2, projY2;
     float projX3, projY3;
-    for(int i=1;i<m_size-1;i++) {
-      for(int j=1;j<m_size-1;j++) {
-	l->ProjectPoint(m_cells_bottom_left[i*m_size*2+2*j],m_cells_bottom_left[i*m_size*2+2*j+1], &projX0, &projY0);
-	l->ProjectPoint(m_cells_bottom_left[(i+1)*m_size*2+2*j],m_cells_bottom_left[(i+1)*m_size*2+2*j+1], &projX1, &projY1);
-	l->ProjectPoint(m_cells_bottom_left[(i+1)*m_size*2+2*(j+1)],m_cells_bottom_left[(i+1)*m_size*2+2*(j+1)+1], &projX2, &projY2);
-	l->ProjectPoint(m_cells_bottom_left[i*m_size*2+2*(j+1)],m_cells_bottom_left[i*m_size*2+2*(j+1)+1], &projX3, &projY3);
-	DrawFilledQuadTangle(image,
-			     (int)projX0, (int)projY0,
-			     (int)projX1, (int)projY1,
-			     (int)projX2, (int)projY2,
-			     (int)projX3, (int)projY3,
-			     m_coder.NextChunk() == 1 ? black : white);
-      }
+    for(int i=0;i<m_length;i++) {
+      l->ProjectPoint(m_cells_corner[2*i],m_cells_corner[2*i+1], &projX0, &projY0);
+      l->ProjectPoint(m_cells_corner[2*i]+m_cell_width,m_cells_corner[2*i+1], &projX1, &projY1);
+      l->ProjectPoint(m_cells_corner[2*i]+m_cell_width,m_cells_corner[2*i+1]+m_cell_width, &projX2, &projY2);
+      l->ProjectPoint(m_cells_corner[2*i],m_cells_corner[2*i+1]+m_cell_width, &projX3, &projY3);
+      DrawFilledQuadTangle(image,
+			   (int)projX0, (int)projY0,
+			   (int)projX1, (int)projY1,
+			   (int)projX2, (int)projY2,
+			   (int)projX3, (int)projY3,
+			   m_coder.NextChunk() == 1 ? black : white);
     }
   }
 
-  virtual unsigned long Decode(Image *image, const Rectangle2D *l) { return 0; }
+
+  virtual unsigned long long Decode(Image *image, const Rectangle2D *l) { 
+#ifdef IMAGE_DEBUG
+    Image* debug0 = cvCloneImage(image);
+    cvConvertScale(debug0,debug0,0.5,128);    
+#endif
+    float projX, projY;
+    m_coder.Reset();
+    // iterate over the tag reading each section
+    for(int i=0;i<m_length;i++) {
+      l->ProjectPoint(m_cells_corner[2*i]+m_cell_width_2,m_cells_corner[2*i+1]+m_cell_width_2, &projX, &projY);
+      try {
+	m_coder.LoadChunk( (SampleImage(image,(int)projX,(int)projY) < 128) ? 1 : 0);
+      }
+      catch (Coder::InvalidSymbol &e) {
+#ifdef IMAGE_DEBUG
+	cvSaveImage("debug-decode0.jpg",debug0);
+	cvReleaseImage(&debug0);
+#endif
+	throw e;
+      }
+#ifdef IMAGE_DEBUG
+      if (i==1) {
+	cvLine(debug0,cvPoint((int)projX,(int)projY),cvPoint((int)projX,(int)projY),255,8);
+	cvLine(debug0,cvPoint((int)projX,(int)projY),cvPoint((int)projX,(int)projY),0,6);
+      }
+      else {
+	cvLine(debug0,cvPoint((int)projX,(int)projY),cvPoint((int)projX,(int)projY),255,4);
+	cvLine(debug0,cvPoint((int)projX,(int)projY),cvPoint((int)projX,(int)projY),0,3);
+      }
+#endif	
+    }
+    
+#ifdef IMAGE_DEBUG
+    cvSaveImage("debug-decode0.jpg",debug0);
+    cvReleaseImage(&debug0);
+#endif
+    return m_coder.Decode();
+  }
 
 };
 #endif//MATRIX_TAG_GUARD
