5eb6dc40fc3b0f0724dee3c178a4a4b8f84dfcc8
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2004-02-03 21:50:34 +0000

    log filtering
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@141 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ include/logfilter.hh
@@ -0,0 +1,17 @@
+/**
+ * $Header$
+ *
+ * $Log$
+ * Revision 1.1  2004/02/03 21:50:34  acr31
+ * log filtering
+ *
+ *
+ */
+#ifndef LOG_FILTER_GUARD
+#define LOG_FILTER_GUARD
+
+#include "Drawing.hh"
+
+void LogFilter(Image *image);
+
+#endif//LOG_FILTER_GUARD
