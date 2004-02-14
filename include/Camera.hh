99a359e0231cc29bb4b9114f6b08813e51110a4f
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2004-02-14 16:23:33 +0000

    renamed functions
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@157 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/Camera.hh
+++ include/Camera.hh
@@ -31,14 +31,48 @@ public:
    * co-ordinates
    */ 
   void SetExtrinsic(float extrinsic[9]);
+
+  /**
+   * Set the intrinsic parameters for this camera. This is a matrix
+   * containing x and y scale factors, principle point and skew
+   */
   void SetIntrinsic(float intrinsic[9]);
+
+  /**
+   * Set the intrinsic parameters for this camera to the given values.
+   */
   void SetIntrinsic(float xscale, float yscale, float principlex, float principley, float skew);
+
+  /**
+   * Set the radial distortion parameters.  This method accepts
+   * co-efficients for the radius squared, to the four and to the six.
+   */
   void SetRadial(float r2,float r4,float r6);
+
+  /**
+   * Set the tangential distortion co-efficients for this camera.
+   */
   void SetTangential(float d1, float d2);
 
-  void MapPoints(float* points, int num_points);
-  void UnMapPoints(float* points, int num_points);  
+  /**
+   * Convert a set of image co-ordinates to normalised principle
+   * co-ordinate frame (NPCF) points.  This involves removing the
+   * distortion predicted by the parameters set and removing the
+   * extrinsic parameters of the camera.
+   */
+  void ImageToNPCF(float* points, int num_points);
 
+  /**
+   * Convert a set of normalised principle co-ordinate frame (NPCF)
+   * points to image points.  This involves applying the correct
+   * distortion predicted by the camera parameters and applying the
+   * extrinsic parameters of the camera.
+   */
+  void NPCFToImage(float* points, int num_points);  
+
+  /**
+   * Remove radial and tangential distortion from the whole image.
+   */
   void UnDistortImage(Image* image);
  
 };
