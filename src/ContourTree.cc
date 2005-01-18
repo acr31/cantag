888644b31cecaeba67491ab0a86d227cde6c017d
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-01-18 17:34:47 +0000

    *** empty log message ***
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@298 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ src/ContourTree.cc
@@ -0,0 +1,333 @@
+/**
+ * $Header$
+ */
+
+#include <ContourTree.hh>
+
+#undef CONTOUR_TREE_DEBUG
+
+Image* debug_image;
+
+ContourTree::ContourTree(Image& image, std::vector<ContourConstraint>& constraints) :
+  m_image(image),
+  m_constraints(constraints)
+{
+
+  unsigned char* data_pointer = image.GetDataPointer();
+  int image_width = image.GetWidth();
+  int image_height = image.GetHeight();
+  int image_width_1 = image_width-1;
+
+  // our frame border is a hole border.  We write zero's around the
+  // edge of the image to make sure that our border follower never
+  // goes out of range
+  for(int i=0;i<image_width;++i) {
+    *data_pointer = 0;
+    data_pointer++;
+  }
+  for(int i=0;i<image_height-2;++i) {
+    *data_pointer = 0;
+    data_pointer+=image_width_1;
+    *data_pointer = 0;
+    ++data_pointer;
+  }
+  for(int i=0;i<image_width;++i) {
+    *data_pointer = 0;
+    ++data_pointer;
+  }  
+
+#ifdef IMAGE_DEBUG
+  image.Save("debug-scenegraph-borders.bmp");
+  debug_image= new Image(image.GetWidth(),image.GetHeight());
+#endif
+
+  m_node_hash[1] = new Contour(1);
+  Contour* current = m_node_hash[1];
+  current->bordertype=HOLE_BORDER;
+  data_pointer = image.GetDataPointer();
+  int NBD = 2;
+  m_node_hash[NBD] = new Contour(NBD);
+  current = m_node_hash[NBD];
+  
+  // we're going to mark the image with contour id's as we find them
+  // the LSB will indicate if we have a exit or an entry pixel
+  // the remainder is the NBD
+  // (note that we only ever mark things that are 1-pixels)
+
+  for(int raster_y=0;raster_y < image_height; ++raster_y) {
+#ifdef CONTOUR_TREE_DEBUG
+    PROGRESS("Updating LNBD to 1 (row start)");
+#endif
+    int LNBD = 1; // we've just "seen" the frame border so set the last seen border id to match
+    ++data_pointer; // exclude the first pixel on the line
+    for(int raster_x=1;raster_x < image_width_1;++raster_x, ++data_pointer) {
+      //      PROGRESS("SCAN " << raster_x << "," << raster_y);
+      if (*data_pointer) {  // this pixel is a 1-element or it has been visited before
+	const int cNBD = *data_pointer>>1;   // left shift to discard the pixel value (or exit/entry flag if already marked)
+	if (cNBD) { // this pixel has been seen before and it is not an exit pixel
+	  //	if (cNBD && (*data_pointer & 0x1)) { // this pixel has been seen before and it is not an exit pixel
+#ifdef CONTOUR_TREE_DEBUG
+	  PROGRESS("Updating LNBD to " << cNBD);
+#endif
+	  LNBD = cNBD;
+	}
+
+	int contour_length;
+	ContourStatistics contour_statistics;;
+	if (!cNBD && !(*(data_pointer-1))) { // this pixel has not been seen before and the previous pixel is a 0-element
+	  current->bordertype = OUTER_BORDER;	  
+#ifdef CONTOUR_TREE_DEBUG
+	  PROGRESS("Found outer border.  Following from " << raster_x << "," << raster_y);
+#endif
+	  contour_length = FollowContour(image, data_pointer, raster_x, raster_y, current->points, contour_statistics,0, NBD);
+	} 
+	else if ((*data_pointer & 0x1) && !(*(data_pointer+1))) { // this pixel has not been seen before or it is not an exit pixel, and the next pixel is a 0-element
+	  current->bordertype = HOLE_BORDER;
+#ifdef CONTOUR_TREE_DEBUG
+	  PROGRESS("Found hole border.  Following from " << raster_x << "," << raster_y);
+#endif
+	  contour_length = FollowContour(image, data_pointer, raster_x, raster_y, current->points, contour_statistics,4, NBD);
+	}
+	else {
+	  continue;
+	}
+
+	// now decide the parent of this border
+	    
+	// NewBorder    LNBDType   Parent
+	// --------------------------------------
+	// OUTER        OUTER      Parent of LNBD
+	// OUTER        HOLE       LNBD
+	// HOLE         OUTER      LNBD
+	// HOLE         HOLE       Parent of LNBD
+	current->parent_id = current->bordertype == m_node_hash[LNBD]->bordertype ?  m_node_hash[LNBD]->parent_id : LNBD;
+#ifdef JAM
+	int size = current->points.size();
+	float* pointsarray = new float[size];
+	int ptr = 0;
+	for(std::vector<float>::const_iterator i = current->points.begin();i!=current->points.end(); ++i) {
+	  pointsarray[ptr++] = *i;
+	}
+	debug.DrawPolygon(pointsarray,size,0,1);
+	delete[] pointsarray;
+#endif
+
+	if (current->parent_id != NBD) {
+	  m_node_hash[current->parent_id]->children.push_back(current);
+#ifdef CONTOUR_TREE_DEBUG
+	  PROGRESS("Adding contour "<<NBD <<" as child of " << current->parent_id);
+#endif
+	}
+
+	NBD = (NBD + 1) & 0x7F;
+	if (NBD==0) { ++NBD; }	 
+
+	current = new Contour(NBD);
+	m_node_hash[NBD] = current;
+
+      }
+      else { // this is a 0-element
+
+      }
+    }
+    ++data_pointer; // exclude the last pixel on the line
+  }
+  
+#ifdef IMAGE_DEBUG
+  debug_image->Save("debug-contourtree-contours.bmp");
+#endif
+}
+
+
+int ContourTree::FollowContour(Image& image, // the image to track the contour in (will be altered)
+			       unsigned char* data_pointer,
+			       int start_x,  int start_y, // the start position (must lie on contour)
+			       std::vector<float>& points,  // the buffer to store the points
+			       ContourStatistics& statistics,   // contour statistics structure (can be NULL)
+			       int start_position,  // the position in the 8-connected region to start searching from
+			       int nbd  // the NBD to mark this contour with
+			       ) {
+  int image_width = image.GetWidth();
+
+  //   +---+---+---+
+  //   | 7 | 6 | 5 |
+  //   +---+---+---+
+  //   | 0 |   | 4 |
+  //   +---+---+---+
+  //   | 1 | 2 | 3 |
+  //   +---+---+---+
+  const int offset_x[] = { -1,-1,0,1,1,1,0,-1 };
+  const int offset_y[] = { 0,1,1,1,0,-1,-1,-1 };
+
+  const int offset[] = {-1,                //  0 
+			image_width-1,     //  1
+			image_width,       //  2
+			image_width+1,     //  3
+			1,                 //  4
+			1-image_width,     //  5
+			-image_width,      //  6
+			-image_width-1};   //  7
+
+
+  // data_pointer is the current focus of the search region
+  // sample_pointer is the current read point
+  unsigned char* sample_pointer;
+
+  // position is our current index into the search region
+  int position = start_position;
+
+  // contour_0 is the first pixel in the contour
+  const unsigned char* contour_0 = data_pointer;
+
+  int NBD_shift = nbd << 1;
+
+  do {
+    position = (position - 1) & 0x7;
+    sample_pointer = data_pointer+offset[position];
+    if (*sample_pointer) { break; }
+  }
+  while (position != start_position);
+
+#ifdef CONTOUR_TREE_DEBUG
+  PROGRESS("Finished clockwise scan at " << position << " (started:" << start_position << ")");
+#endif
+
+  if (position == start_position) {
+    // one pixel contour
+    *data_pointer = NBD_shift;
+    points.push_back(start_x);
+    points.push_back(start_y);
+#ifdef CONTOUR_TREE_DEBUG
+    PROGRESS("Found 1 pixel contour starting from "<< start_x << "," << start_y);
+#endif
+#ifdef IMAGE_DEBUG
+    debug_image->DrawPixel(start_x,start_y,COLOUR_BLACK);
+#endif
+    return 1;
+  }
+  else {
+    position = (position + 1) & 0x7;
+
+    // contour_n is the last pixel in the contour
+    const unsigned char* contour_n = sample_pointer;
+
+    bool cell4_is_0 = false; // will be set to true when we search a region if we pass cell4 and cell4 is a 0-element
+    
+    statistics.length = 0; // the contour length
+    statistics.min_x = start_x; // bounding box
+    statistics.max_x = start_x; // bounding box
+    statistics.min_y = start_y; // bounding box
+    statistics.max_y = start_y; // bounding box
+    statistics.convex = true; // true if this contour is convex
+
+    while(1) {
+      sample_pointer = data_pointer+offset[position];
+      if (*sample_pointer) {
+#ifdef CONTOUR_TREE_DEBUG
+	PROGRESS("Found 1-pixel at position " << position << " around " << start_x << "," << start_y);
+#endif
+#ifdef IMAGE_DEBUG
+	debug_image->DrawPixel(start_x,start_y,128);
+#endif
+	// we now need to mark this pixel
+	// 1) if the pixel sample_x+1,sample_y (cell 4) is a 0-element and we
+	// have examined it whilst looking for this 1-element then this
+	// is an exit pixel.  Write (NBD,r).
+	if (cell4_is_0) {
+	  *data_pointer = NBD_shift;
+#ifdef CONTOUR_TREE_DEBUG
+	  PROGRESS("Marked  exit " << start_x << "," << start_y << " with " << NBD_shift);
+#endif
+	}
+	// 2) else if sample_x,sample_y is unmarked write
+	// (NBD,l).
+	//	else if (!(*sample_pointer & ~0x1)) {
+	else if (!(*data_pointer & ~0x1)) {
+	  *data_pointer = NBD_shift | 0x1;
+#ifdef CONTOUR_TREE_DEBUG
+	  PROGRESS("Marked " << start_x << "," << start_y << " with " << (NBD_shift | 0x1));
+#endif
+	}
+
+	// update the length
+	statistics.length += (position & 0x1 ? 45 : 32);
+	// update the bounding box
+	if (start_x < statistics.min_x) { statistics.min_x = start_x; }
+	else if (start_x > statistics.max_x) { statistics.max_x = start_x; }
+	if (start_y < statistics.min_y) { statistics.min_y = start_y; }
+	else if (start_y > statistics.max_y) { statistics.max_y = start_y; }
+
+	// check the stopping condition
+	if ((points.size() > 4) &&
+	    (data_pointer == contour_n) &&
+	    (sample_pointer == contour_0)) {
+	  statistics.length >>= 5;
+#ifdef CONTOUR_TREE_DEBUG
+	  PROGRESS("Found " << (points.size()>>1) << " pixel contour starting from "<< points[0] << "," << points[1]);
+#endif
+#ifdef IMAGE_DEBUG
+	  debug_image->DrawPixel(start_x,start_y,0);		  
+#endif
+	  return points.size()>>1;
+    	}
+
+	// store this point in the pixel chain and update the start position
+	points.push_back(start_x);
+	points.push_back(start_y);
+	start_x += offset_x[position];
+	start_y += offset_y[position];
+	data_pointer = sample_pointer;
+
+	// reset the search value for cell 4
+	cell4_is_0=false;
+
+	// if we find the 1-element at position n anti-clockwise then we
+	// need to shift the region to be centered on the new point and
+	// resume searching one place after the previous central point
+	// i.e. if we find a point at 3 we need to resume searching from
+	// 0 (one place past the old centre at 7)
+	
+	//    +---+---+---+---+         +---+---+---+---+   
+	//    | 7 | 6 | 5 |   |         |   |   |   |   |
+	//    +---+---+---+---+         +---+---+---+---+   
+	//    | 0 |   | 4 |   |         |   |_7_| 6 | 5 |
+	//    +---+---+---+---+   -->   +---+---+---+---+   
+	//    | 1 | 2 | X |   |         |   | 0 | X | 4 |
+	//    +---+---+---+---+         +---+---+---+---+   
+	//    |   |   |   |   |         |   | 1 | 2 | 3 |
+	//    +---+---+---+---+         +---+---+---+---+   
+	
+	//  Found posn (anti)       |  Resume posn (anti)
+	//  -----------------------------------------------
+	//          0               |         5
+	//          1               |         6
+	//          2               |         7
+	//          3               |         0
+	//          4               |         1
+	//          5               |         2
+	//          6               |         3
+	//          7               |         4
+	//
+	position = (position+5) & 0x7;
+      }
+      else {  // pixel is a 0-element
+	if (position == 4) {  // if we are at cell4 (this is a 0-element) then set the cell4_is_0 flag
+	  cell4_is_0 = true; 
+	}
+	
+	// advance the position
+	position = (position+1) & 0x7;
+      }
+
+    }
+
+    statistics.length >>= 5;
+    return (points.size()-3)/2;
+  }
+}
+
+ContourTree::~ContourTree() {
+  for(std::map<int,Contour*>::const_iterator i = m_node_hash.begin();i!=m_node_hash.end();++i) {
+    delete i->second;
+  }
+}
---
+++ unit/TestContourTree.cc
@@ -0,0 +1,159 @@
+/**
+ * $Header$
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <boost/random.hpp>
+#include <ContourTree.hh>
+#include <Image.hh>
+
+#define IMAGE_WIDTH 1000
+#define IMAGE_HEIGHT 600
+
+using boost::unit_test_framework::test_suite;
+ 
+boost::mt19937 rng;
+boost::uniform_int<> widthsource(0,IMAGE_WIDTH);
+boost::uniform_int<> heightsource(0,IMAGE_HEIGHT);
+boost::uniform_int<> childrensource(0,10);
+boost::variate_generator<boost::mt19937, boost::uniform_int<> > widthnumbers(rng, widthsource);
+boost::variate_generator<boost::mt19937, boost::uniform_int<> > heightnumbers(rng, heightsource);
+boost::variate_generator<boost::mt19937, boost::uniform_int<> > children(rng, childrensource);
+
+
+void check_square(std::vector<float>& points,int xpos,int ypos, int xedge,int yedge, bool hole) {
+  int* expected = new int[4*xedge+4*yedge];
+  int ptr = 0;
+  if (hole) {
+    expected[ptr++] = 0;
+    expected[ptr++] = 1;
+    for(int i=1;i<xedge;++i) {
+      expected[ptr++] = i;
+      expected[ptr++] = 0;
+    }
+    for(int i=1;i<yedge;++i) {
+      expected[ptr++] = xedge;
+      expected[ptr++] = i;
+    }
+    for(int i=1;i<xedge;++i) {
+      expected[ptr++] = xedge-i;
+      expected[ptr++] = yedge;
+    }
+    for(int i=1;i<yedge-2;++i) {
+      expected[ptr++] = 0;
+      expected[ptr++] = yedge-i;
+    }
+
+  }
+  else {
+    for(int i=0;i<yedge;++i) {
+      expected[ptr++] = 0;
+      expected[ptr++] = i;
+    }
+    for(int i=0;i<xedge;++i) {
+      expected[ptr++] = i;
+      expected[ptr++] = yedge;
+    }
+    for(int i=0;i<yedge;++i) {
+      expected[ptr++] = xedge;
+      expected[ptr++] = yedge-i;
+    }
+    for(int i=0;i<xedge;++i) {
+      expected[ptr++] = xedge-i;
+      expected[ptr++] = 0;
+    }
+  }
+  ptr = 0;
+  int isx = 1;
+  for(std::vector<float>::const_iterator j = points.begin();j!=points.end();++j) {
+    if (expected[ptr++]+(isx?xpos:ypos) != *j) {	
+      throw "Failed to follow expected contour";
+    }
+    isx^=1;
+  }
+}
+ 
+
+
+void multi_square(int size) {
+  int edge = size-2;
+
+  Image image(IMAGE_WIDTH,IMAGE_HEIGHT);
+  for(int i=2;i<IMAGE_WIDTH-size;i+=size) {
+    for(int j=2;j<IMAGE_HEIGHT-size;j+=size) {
+      image.DrawFilledQuadTangle(i,j,
+				 i+edge,j,
+				 i+edge,j+edge,
+				 i,j+edge,
+				 COLOUR_BLACK);
+      image.DrawFilledQuadTangle(i+1,j+1,
+             			 i+edge/2-1,j+1,
+				 i+edge/2-1,j+edge-1,
+				 i+1,j+edge-1,
+				 COLOUR_WHITE);				 
+      image.DrawFilledQuadTangle(i+edge/2+5,j+5,
+      				 i+edge-1,j+5,
+				 i+edge-1,j+edge-1,
+				 i+edge/2+5,j+edge-1,
+				 COLOUR_WHITE);
+
+      image.DrawFilledQuadTangle(i+5,j+5,
+				 i+8,j+5,
+				 i+8,j+8,
+				 i+5,j+8,
+				 COLOUR_BLACK);
+    }
+  }
+  image.Save("temp.bmp");
+  image.GlobalThreshold(128);
+  std::vector<ContourTree::ContourConstraint> v;
+  ContourTree c(image,v);
+  
+ 
+
+  ContourTree::Contour* root = c.GetRootContour();
+  int xpos = 2;
+  int ypos = 2;
+  for(std::vector<ContourTree::Contour*>::const_iterator i = root->children.begin(); i!=root->children.end();++i) {    
+    ContourTree::Contour* current = *i;
+    check_square(current->points,xpos,ypos,edge,edge,false);
+    if (current->children.size() != 2) {
+      throw "Wrong number of child contours";
+    }
+    check_square(current->children[0]->points,xpos,ypos,edge/2,edge,true);
+    check_square(current->children[1]->points,xpos+edge/2+4,ypos+4,edge/2-4,edge-4,true);
+    if (current->children[0]->children.size() != 1) {
+      throw "Wrong number of child contours";
+    }
+    check_square(current->children[0]->children[0]->points,xpos+5,ypos+5,3,3,false);
+    xpos+=size;
+    if (xpos >= IMAGE_WIDTH -size) {
+      ypos+=size;
+      xpos=2;
+    }
+  }
+
+}
+
+void test_multi_square() {
+  multi_square(200);
+}
+
+void test_circle() {
+  
+  Image i(IMAGE_WIDTH,IMAGE_HEIGHT);
+  //x  draw_circle(i);
+
+
+}
+
+test_suite*
+init_unit_test_suite( int argc, char* argv[] )
+{
+  test_suite* test= BOOST_TEST_SUITE( "ContourTree test" );
+
+  test->add( BOOST_TEST_CASE( &test_multi_square ) );
+
+  return test;
+}
+
