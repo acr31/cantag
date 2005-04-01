e9fff4e735e9f98cbf94a3bb22a0fe4a4efa26ab
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-04-01 15:59:44 +0000

    fixes to the output mechanism
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@451 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/total/ImageOutputMechanism.hh
+++ include/total/ImageOutputMechanism.hh
@@ -12,17 +12,18 @@ namespace Total {
   class ImageOutputMechanism {
   private:
     const Camera& m_camera;
-    Image m_savedimage;
+    Image* m_savedimage;
   public:
   
-    ImageOutputMechanism(const Camera& camera) : m_camera(camera) {};
+    ImageOutputMechanism(const Camera& camera) : m_camera(camera), m_savedimage(NULL) {};
   
     inline void FromImageSource(const Image& image) {
-      m_savedimage = image;
-      image->Save("debug-fromimagesource.pnm");
+      image.Save("debug-fromimagesource.pnm");
+
+      m_savedimage = new Image(image);
     };
     inline void FromThreshold(const Image& image) {
-      image->Save("debug->fromthreshold.pnm");
+      image.Save("debug-fromthreshold.pnm");
     };
     inline void FromContourTree(const ContourTree& contours) {};
     inline void FromRemoveIntrinsic(const ContourTree& contours) {};
@@ -30,7 +31,11 @@ namespace Total {
     template<int PAYLOADSIZE> void FromTag(const WorldState<PAYLOADSIZE>& world);
   };
   
-  template<int PAYLOADSIZE> void TextOutputMechanism::FromTag(const WorldState<PAYLOADSIZE>& world) {
+  template<int PAYLOADSIZE> void ImageOutputMechanism::FromTag(const WorldState<PAYLOADSIZE>& world) {
+    if (!m_savedimage) throw "Must call FromImageSource before calling FromTag";
+
+    m_savedimage->ConvertScale(0.25,0);
+    m_savedimage->ConvertScale(1,128);      
     for(typename std::vector<LocatedObject<PAYLOADSIZE>*>::const_iterator i = world.GetNodes().begin();
 	i != world.GetNodes().end();
 	++i) {
@@ -40,11 +45,13 @@ namespace Total {
 		       -1,1,
 		       1,1,
 		       1,-1};
-	ApplyTransform(lobj->transform,pts,4);
+	ApplyTransform(loc->transform,pts,4);
 	m_camera.NPCFToImage(pts,4);
-	m_savedimage.DrawPolygon(pts,4,COLOUR_BLACK,1);
+	m_savedimage->DrawPolygon(pts,4,COLOUR_BLACK,1);
       }
     }
+    m_savedimage->Save("debug-fromtag.pnm");
+    delete m_savedimage;
   }
 }
 #endif//IMAGE_OUTPUT_MECHANISM_GUARD
