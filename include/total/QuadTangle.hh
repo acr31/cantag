9cf47c795f62591821750da38885f1d89be9ee31
rkh23 <rkh23@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-04-04 09:05:11 +0000

    Added RegressionQuadTangle
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@465 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/total/QuadTangle.hh
+++ include/total/QuadTangle.hh
@@ -77,6 +77,47 @@ namespace Total {
     bool Fit(const std::vector<float>& points);
   };
 
+  class RegressionQuadTangle : public QuadTangle {
+  public:
+    RegressionQuadTangle();
+    RegressionQuadTangle(const std::vector<float>& points, bool prev_fitted=false);
+  protected:
+    /**
+     * Take the set of points and form a convex hull
+     * to smooth out quantization 'kinks'. Filter out
+     * vertices clearly not corners based on angles.
+     * What remains should be clusters of points at
+     * the true corners. Take one from each cluster
+     * as an _estimate_ of that corner position.
+     * Use the estimates to split the original poly
+     * up into 4 groups, one per side. Regress the
+     * groups linearly and find the intersection of
+     * the lines
+     */
+    bool Fit(const std::vector<float>& points);
+
+  private:
+
+    /**
+     * Check where a point p lies relative to a line
+     * specified by the points l0 and l1.
+     * >0 means p is on the left
+     * =0 means on the line
+     * <0 means to the right
+     */
+    float isLeft( const std::vector<float> &V, int l0, int l1, int p);
+
+    /**
+     * Get the convex hull of the n points in V. This assumes
+     * H has been allocated to size n.
+     * The _indexes_ to the points are then returned in H, and
+     * the number of vertices (<=n) is also returned.
+     */
+    int ConvexHull(const std::vector<float> &V, int n, int* H);
+    
+  };
+
+
   class PolygonQuadTangle : public QuadTangle {
   private:
     /**
