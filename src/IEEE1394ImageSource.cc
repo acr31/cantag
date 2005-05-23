f43583e46049fc53c26702bdf4e536d5be27fccc
rkh23 <rkh23@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-05-23 08:54:05 +0000

    Added features to the constructor and some minimal exception throwing.
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@551 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/IEEE1394ImageSource.cc
+++ src/IEEE1394ImageSource.cc
@@ -4,17 +4,7 @@
 
 #include <total/IEEE1394ImageSource.hh>
 
-extern "C" {
-#include <sys/types.h>  // for open()
-#include <sys/stat.h>   // for open()
-#include <fcntl.h>      // for open()
-#include <sys/ioctl.h>  // for ioctl()
-#include <unistd.h>     // for mmap()
-#include <sys/mman.h>   // for mmap()
-}
-
 #include <cerrno>
-#include <iostream>
 
 #undef IEEE1394_DEBUG
 
@@ -24,7 +14,10 @@ namespace Total {
   IEEE1394ImageSource::IEEE1394ImageSource(
 					   char *device_name,
 					   int port,
-					   u_int64_t guid
+					   u_int64_t guid,
+					   int framerate,
+					   int shutter,
+					   int gain
 					   ) :mImage(0)
   {
     
@@ -33,43 +26,43 @@ namespace Total {
 
      int nCam = 0;
      nodeid_t * camera_nodes = dc1394_get_camera_nodes(mHandle, &nCam, 0);
-     /*
-       \todo
-       if (nCam==0) ERROR
-     */
+     
+     if (nCam==0) {
+       throw "Unable to find a 1394 Camera. Please check that you have modprobed raw1394 and videodev1394 and created the relevant device nodes in /dev";
+     }
+
+
      if (nCam>1) {
        // More than one camera - use guid to select
        dc1394_camerainfo info;
+       bool found = false;
        for (int i=0; i<nCam; i++) {
 	 if (dc1394_get_camera_info(mHandle, camera_nodes[i], &info) == DC1394_SUCCESS) {
 	   if (info.euid_64 == guid) {
 	     dc1394_print_camera_info(&info);
 	     mCamera.node = camera_nodes[i];
+	     found=true;
 	     break;
 	   }
 	 }
        }
+       if (!found) throw "Unable to find a 1394 Camera. Please check that you have modprobed raw1394 and videodev1394 and created the relevant device nodes in /dev";
      } 
      else {
        mCamera.node = camera_nodes[0];
      }
      dc1394_free_camera_nodes(camera_nodes);
+
+     if (shutter>=0) dc1394_set_shutter(mHandle, mCamera.node,shutter);
+     if (gain>=0) dc1394_set_gain(mHandle, mCamera.node,gain);
      
      // Now we want to set up the capture parameters
-     // \todo check this call!
-    //  dc1394_setup_capture(mHandle, mCamera.node,
-// 			  0, /* channel */ 
-// 			  FORMAT_VGA_NONCOMPRESSED,
-// 			  MODE_640x480_MONO,
-// 			  SPEED_400,
-// 			  FRAMERATE_7_5,
-// 			  &mCamera);
      dc1394_dma_setup_capture(mHandle, mCamera.node,
 			      0, /* channel */ 
 			      FORMAT_VGA_NONCOMPRESSED,
 			      MODE_640x480_MONO,
 			      SPEED_400,
-			      FRAMERATE_30,
+			      framerate,
 			      8,1,  
 			      device_name,
 			      &mCamera);
@@ -102,19 +95,17 @@ namespace Total {
 
     // Doing a copy instead at the moment!!
     if (!mImage) {
-      mImage = new Image(640,480);
+      mImage = new Image(GetWidth(),GetHeight());
     }
     dc1394_dma_single_capture(&mCamera);
     unsigned char *img = (unsigned char*) mCamera.capture_buffer;
-    for (int i=0; i<640; i++) {
-      for (int j=0; j<480; j++) {
-	mImage->DrawPixelNoCheck(i,j,img[i+j*640]);
+    for (int i=0; i<GetWidth(); i++) {
+      for (int j=0; j<GetHeight(); j++) {
+	mImage->DrawPixelNoCheck(i,j,img[i+j*GetWidth()]);
       }
     }
     dc1394_dma_done_with_buffer(&mCamera);
     return mImage;
   }
-
-
 
 }
