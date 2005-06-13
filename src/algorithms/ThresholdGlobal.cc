f11e33912a54b3816140fb2a127a2a82b1d9b7b0
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-06-13 14:00:52 +0000

    wrote black border around image
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@571 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/algorithms/ThresholdGlobal.cc
+++ src/algorithms/ThresholdGlobal.cc
@@ -14,8 +14,18 @@ namespace Total {
       const unsigned char* data_pointer = image.GetRow(i);
       for(int j=0;j<width;++j) {
 	const unsigned char pixel = *data_pointer;
-	dest.SetPixel(j,i, pixel > m_threshold);
+	dest.SetPixel(j,i, pixel < m_threshold);
 	++data_pointer;
+      }
+    }
+
+    for(int i=0;i<height;++i) {
+      dest.SetPixel(0,i,false);
+      dest.SetPixel(width-1,i,false);
+      if (i == 0 || i == height-1) {
+	for(int j=0;j<width-1;++j) {
+	  dest.SetPixel(j,i,false);
+	}
       }
     }
     return true;
