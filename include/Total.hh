cf6f35abda0829e61d191cbae741b25a5b42ecad
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-06-14 17:01:35 +0000

    reordered and commented
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@585 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/Total.hh
+++ include/Total.hh
@@ -9,47 +9,75 @@
 
 #include <total/Config.hh>
 
-// basic tag code
-#include <total/RingTag.hh>
-#include <total/MatrixTag.hh>
+#include <total/Entity.hh>
+#include <total/ComposeEntity.hh>
+#include <total/EntityTree.hh>
 
+#include <total/Apply.hh>
+#include <total/Bind.hh>
+
+///////////////////////
+// fundamental tag types
+#include <total/TagCircle.hh>
+#include <total/TagSquare.hh>
+
+///////////////////////
+// algorithms
+#include <total/algorithms/FitEllipseLS.hh>
+#include <total/algorithms/FitEllipseSimple.hh>
+#include <total/algorithms/FitQuadTangleCorner.hh>
+#include <total/algorithms/FitQuadTangleConvexHull.hh>
+#include <total/algorithms/FitQuadTanglePolygon.hh>
+#include <total/algorithms/DistortionCorrection.hh>
+#include <total/algorithms/TransformEllipseLinear.hh>
+#include <total/algorithms/TransformEllipseFull.hh>
+#include <total/algorithms/TransformQuadTangleProjective.hh>
+#include <total/algorithms/TransformQuadTangleReduced.hh>
+#include <total/algorithms/TransformQuadTangleSpaceSearch.hh>
+#include <total/algorithms/SampleTagCircle.hh>
+#include <total/algorithms/SampleTagSquare.hh>
+#include <total/algorithms/ContourFollowerTree.hh>
+#include <total/algorithms/Decode.hh>
+#include <total/algorithms/ThresholdAdaptive.hh>
+#include <total/algorithms/ThresholdGlobal.hh>
+
+///////////////////////
 // coders
 #include <total/coders/RawCoder.hh>
 #include <total/coders/SCCCoder.hh>
 #include <total/coders/ParityCoder.hh>
 #include <total/coders/SCCCoder.hh>
 #include <total/coders/SymbolChunkCoder.hh>
-#if defined(HAVE_GMPXX) && defined(HAVELIB_GMP) && defined(HAVELIB_GMPXX)
+#if defined(HAVE_GMPXX_H) && defined(HAVELIB_GMP) && defined(HAVELIB_GMPXX)
 # include <total/coders/TripOriginalCoder.hh>
 #endif
 
+//////////////////////
 // image sources
 #include <total/FileImageSource.hh>
 #ifdef HAVE_LINUX_VIDEODEV_H
 # include <total/V4LImageSource.hh>
 #endif
+#if defined(HAVE_GL_GL_H) && defined(HAVE_GL_GLU_H) && defined(HAVE_GL_OSMESA_H) && defined(HAVELIB_GL) && defined(HAVELIB_OSMESA) && defined(HAVELIB_GLU)
+# include <total/GLImageSource.hh>
+#endif
+#if defined(HAVELIB_DC1394) && defined(HAVE_DC1394_CONTROL_H)
+#include <total/IEEE1394ImageSource.hh>
+#endif
 
+//////////////////////
 // output mechanisms
-#include <total/TextOutputMechanism.hh>
-#include <total/ImageOutputMechanism.hh>
+// #include <total/TextOutputMechanism.hh>
+// #include <total/ImageOutputMechanism.hh>
 #if defined(HAVE_X11_XLIB_H) && defined(HAVE_X11_XUTIL_H) && defined(HAVE_X11_EXTENSIONS_XSHM_H)
 # include <total/XOutputMechanism.hh>
 //# include <total/XOutputStagesMechanism.hh>
 #endif
 
 #if defined(HAVE_GL_GL_H) && defined(HAVELIB_GL) && defined(HAVELIB_GLU) && defined(HAVELIB_GLUT)
-# include <total/GLOutputMechanism.hh>
+//# include <total/GLOutputMechanism.hh>
 #endif
 
-#if defined(HAVE_GL_GL_H) && defined(HAVE_GL_GLU_H) && defined(HAVE_GL_OSMESA_H) && defined(HAVELIB_GL) && defined(HAVELIB_OSMESA) && defined(HAVELIB_GLU)
-# include <total/GLImageSource.hh>
-#endif
 
-#if defined(HAVELIB_DC1394) && defined(HAVE_DC1394_CONTROL_H)
-#include <total/IEEE1394ImageSource.hh>
-#endif
-
-// some predefined tags
-#include <total/Tags.hh>
 
 #endif//TOTAL_GUARD
