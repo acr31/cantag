51dc9db3363e8a6221d7857b534b6f408aacfe5e
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-07-15 10:43:19 +0000

    algorithm for roatating a circular tag transform to snap to a sector edge
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@617 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ include/total/algorithms/TransformEllipseRotate.hh
@@ -0,0 +1,100 @@
+/**
+ * $Header$
+ */
+
+#ifndef TRANSFORM_ELLIPSE_ROTATE_GUARD
+#define TRANSFORM_ELLIPSE_ROTATE_GUARD
+
+#include <total/Function.hh>
+#include <total/MonochromeImage.hh>
+#include <total/entities/TransformEntity.hh>
+
+namespace Total {
+
+  /**
+   * An algorithm to rotate the object co-ordinates of the transforms
+   * to line up with a sector edge.  This is a helper object that may
+   * be constructed using the templated function
+   * TransformEllipseRotate which will infer template parameters
+   * automatically
+   */
+  template<int RING_COUNT, int SECTOR_COUNT, int READ_COUNT=5>
+  class TransformEllipseRotateObj : public Function1<MonochromeImage,TransformEntity> {
+  private:
+    const TagCircle<RING_COUNT,SECTOR_COUNT,READ_COUNT>& m_tagspec;
+    const Camera& m_camera;
+  public:
+    TransformEllipseRotateObj(const TagCircle<RING_COUNT,SECTOR_COUNT,READ_COUNT>& tagspec, const Camera& camera) : m_tagspec(tagspec),m_camera(camera){}
+    bool operator()(const MonochromeImage& image, TransformEntity& transform) const;
+  };
+
+  template<int RING_COUNT, int SECTOR_COUNT, int READ_COUNT> bool TransformEllipseRotateObj<RING_COUNT,SECTOR_COUNT,READ_COUNT>::operator()(const MonochromeImage& image, TransformEntity& source) const {
+
+    for(std::list<Transform*>::const_iterator i = source.GetTransforms().begin(); i != source.GetTransforms().end(); ++i) {
+      Transform* transform = *i;
+      bool left = true;
+      bool right = true;
+      int k=RING_COUNT-1;
+      int j;
+      // scan round reading a half sector width apart until we read two adjacent cells of different values
+      for(j=0;j<SECTOR_COUNT*READ_COUNT;j+=READ_COUNT/2) {
+	float tpt[]=  {  m_tagspec.GetXSamplePoint(j,k),
+			 m_tagspec.GetYSamplePoint(j,k) };
+	transform->Apply(tpt[0],tpt[1],tpt,tpt+1);
+	m_camera.NPCFToImage(tpt,1);
+	if (tpt[0] < 0 || tpt[0] >= image.GetWidth() ||
+	    tpt[1] < 0 || tpt[1] >= image.GetHeight()) { 
+	  return false;
+	}
+	right = image.GetPixel(tpt[0],tpt[1]);
+	if ((j>0) && (left != right)) break;
+	left = right;
+      }
+  
+      if (left == right) {
+#ifdef RING_TAG_DEBUG
+	PROGRESS("Failed to find a sector edge!");
+#endif
+	continue;
+      }
+  
+      int leftindex = j-READ_COUNT;
+      int rightindex = j;
+      while(rightindex - leftindex > 1) {
+	int centre = (leftindex + rightindex) / 2;
+	float tpt[]=  {  m_tagspec.GetXSamplePoint(centre,k),
+			 m_tagspec.GetYSamplePoint(centre,k) };
+	transform->Apply(tpt[0],tpt[1],tpt,tpt+1);
+	m_camera.NPCFToImage(tpt,1);
+	if (tpt[0] < 0 || tpt[0] >= image.GetWidth() ||
+	    tpt[1] < 0 || tpt[1] >= image.GetHeight()) { 
+	  return false;
+	}
+	bool sample = image.GetPixel(tpt[0],tpt[1]);
+	if (sample) {
+	  if (left && !right) leftindex = centre;
+	  else if (!left && right) rightindex = centre;
+	  else assert(false);
+	}
+	else {
+	  if (left && !right) rightindex = centre;
+	  else if (!left && right) leftindex = centre;
+	  else assert(false);
+	}
+      }
+
+      float cos,sin;
+      m_tagspec.GetAngle(leftindex,cos,sin);
+      transform->Rotate(cos,sin);
+    }
+    return true;
+  }
+
+  template<int RING_COUNT, int SECTOR_COUNT, int READ_COUNT>
+  inline
+  TransformEllipseRotateObj<RING_COUNT,SECTOR_COUNT,READ_COUNT> TransformEllipseRotate(const TagCircle<RING_COUNT,SECTOR_COUNT,READ_COUNT>& tagspec, const Camera& camera) {
+    return TransformEllipseRotateObj<RING_COUNT,SECTOR_COUNT,READ_COUNT>(tagspec,camera);
+  }
+}
+
+#endif//TRANSFORM_ELLIPSE_ROTATE_GUARD
