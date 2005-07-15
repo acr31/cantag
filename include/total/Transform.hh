86ae6ad40552db0055b4f336e535b4a8b3f64e3b
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-07-15 10:39:45 +0000

    added methods for rotating the object co-ordinates
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@607 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/total/Transform.hh
+++ include/total/Transform.hh
@@ -4,27 +4,36 @@
 
 #include <total/Config.hh>
 
-/**
- * A class to represent a transformation from camera co-ordinates to
- * object co-ordinates
- */
-class Transform {
-private:
-  float m_transform[16];
-  float m_confidence;
+namespace Total {
   
-public:
-  
-  Transform(float confidence);
-  Transform(float* transform, float confidence);
-
-  inline float& operator[](int index) { return m_transform[index]; }
-  inline float operator[](int index) const { return m_transform[index]; }
-  
-  inline float getConfidence() { return m_confidence;} 
-  inline void setConfidence(float confidence) { m_confidence = confidence; }
-
-  void Apply(float x, float y, float* resx, float* resy) const;
-  void Apply(float x, float y, float z,float* resx, float* resy) const;
-  void Apply(float* points, int num_points) const;
-};
+  /**
+   * A class to represent a transformation from camera co-ordinates to
+   * object co-ordinates
+   */
+  class Transform {
+  private:
+    float m_transform[16];
+    float m_confidence;
+    
+  public:
+    
+    Transform(float confidence);
+    Transform(float* transform, float confidence);
+    
+    inline float& operator[](int index) { return m_transform[index]; }
+    inline float operator[](int index) const { return m_transform[index]; }
+    
+    inline float getConfidence() { return m_confidence;} 
+    inline void setConfidence(float confidence) { m_confidence = confidence; }
+    
+    void Apply(float x, float y, float* resx, float* resy) const;
+    void Apply(float x, float y, float z,float* resx, float* resy) const;
+    void Apply(float* points, int num_points) const;
+    
+    /**
+     * Rotate the tag's object co-ordinates by a specific angle. Takes
+     * the sine and cos of this angle as arguments.
+     */
+    void Rotate(float cos,float sin);
+  };
+}
--- src/Transform.cc
+++ src/Transform.cc
@@ -4,65 +4,91 @@
 
 #include <total/Transform.hh>
 
-
 #undef TRANSFORM_DEBUG
 
-Transform::Transform(float confidence) : m_confidence(confidence) {};
+namespace Total {
 
-Transform::Transform(float* transform, float confidence) : m_confidence(confidence) {
-  for(int i=0;i<16;++i) {
-    m_transform[i] = transform[i];
+  Transform::Transform(float confidence) : m_confidence(confidence) {};
+
+  Transform::Transform(float* transform, float confidence) : m_confidence(confidence) {
+    for(int i=0;i<16;++i) {
+      m_transform[i] = transform[i];
+    }
   }
-}
 
-void Transform::Apply(float* points, int numpoints) const {
-  for(int i=0;i<numpoints*2;i+=2) {
-    Apply(points[i],points[i+1],points+i,points+i+1);
+  void Transform::Apply(float* points, int numpoints) const {
+    for(int i=0;i<numpoints*2;i+=2) {
+      Apply(points[i],points[i+1],points+i,points+i+1);
+    }
   }
-}
 
-void Transform::Apply(float x, float y, float z, float* projX, float* projY) const {
-  *projX = m_transform[0]*x + m_transform[1]*y + m_transform[2]*z + m_transform[3];
-  *projY = m_transform[4]*x + m_transform[5]*y + m_transform[6]*z + m_transform[7];
-  float projZ = m_transform[8]*x + m_transform[9]*y + m_transform[10]*z + m_transform[11];
-  float projH = m_transform[12]*x + m_transform[13]*y + m_transform[14]*z + m_transform[15];
+  void Transform::Apply(float x, float y, float z, float* projX, float* projY) const {
+    *projX = m_transform[0]*x + m_transform[1]*y + m_transform[2]*z + m_transform[3];
+    *projY = m_transform[4]*x + m_transform[5]*y + m_transform[6]*z + m_transform[7];
+    float projZ = m_transform[8]*x + m_transform[9]*y + m_transform[10]*z + m_transform[11];
+    float projH = m_transform[12]*x + m_transform[13]*y + m_transform[14]*z + m_transform[15];
   
 #ifdef TRANSFORM_DEBUG
-  PROGRESS("Transformed ("<<x<<","<<y<<","<<z<<") on to ("<<*projX<<","<<*projY<<","<<projZ<<","<<projH<<")");
+    PROGRESS("Transformed ("<<x<<","<<y<<","<<z<<") on to ("<<*projX<<","<<*projY<<","<<projZ<<","<<projH<<")");
 #endif
   
-  *projX /= projH;
-  *projY /= projH;
-  projZ /= projH;
+    *projX /= projH;
+    *projY /= projH;
+    projZ /= projH;
   
-  *projX /= projZ;
-  *projY /= projZ;
+    *projX /= projZ;
+    *projY /= projZ;
   
 #ifdef TRANSFORM_DEBUG
-  PROGRESS("Projected ("<<x<<","<<y<<","<<z<<") on to ("<<*projX<<","<<*projY<<")");
+    PROGRESS("Projected ("<<x<<","<<y<<","<<z<<") on to ("<<*projX<<","<<*projY<<")");
 #endif
 
-}
+  }
 
-void Transform::Apply(float x, float y, float* projX, float* projY) const {
-  *projX = m_transform[0]*x + m_transform[1]*y + m_transform[3];
-  *projY = m_transform[4]*x + m_transform[5]*y + m_transform[7];
-  float projZ = m_transform[8]*x + m_transform[9]*y + m_transform[11];
-  float projH = m_transform[12]*x + m_transform[13]*y + m_transform[15];
+  void Transform::Apply(float x, float y, float* projX, float* projY) const {
+    *projX = m_transform[0]*x + m_transform[1]*y + m_transform[3];
+    *projY = m_transform[4]*x + m_transform[5]*y + m_transform[7];
+    float projZ = m_transform[8]*x + m_transform[9]*y + m_transform[11];
+    float projH = m_transform[12]*x + m_transform[13]*y + m_transform[15];
   
 #ifdef TRANSFORM_DEBUG
-  PROGRESS("Transformed ("<<x<<","<<y<<","<<"0) on to ("<<*projX<<","<<*projY<<","<<projZ<<","<<projH<<")");
+    PROGRESS("Transformed ("<<x<<","<<y<<","<<"0) on to ("<<*projX<<","<<*projY<<","<<projZ<<","<<projH<<")");
 #endif
   
-  *projX /= projH;
-  *projY /= projH;
-  projZ /= projH;
+    *projX /= projH;
+    *projY /= projH;
+    projZ /= projH;
   
-  *projX /= projZ;
-  *projY /= projZ;
+    *projX /= projZ;
+    *projY /= projZ;
   
 #ifdef TRANSFORM_DEBUG
-  PROGRESS("Projected ("<<x<<","<<y<<","<<"0) on to ("<<*projX<<","<<*projY<<")");
+    PROGRESS("Projected ("<<x<<","<<y<<","<<"0) on to ("<<*projX<<","<<*projY<<")");
 #endif
 
+  }
+
+  /**
+   * Rotate the tag by angle radians.
+   */
+  void Transform::Rotate(float cos,float sin) {
+    float rotation[16] = { cos, -sin,0,0,
+			   sin, cos,0,0,
+			   0,0,1,0,
+			   0,0,0,1};
+
+    float store[16];
+    for(int i=0;i<16;++i) {
+      store[i] = m_transform[i];
+      m_transform[i] = 0;
+    }
+
+    for(int row=0;row<4;row++) {
+      for(int col=0;col<4;col++) {
+	for(int k=0;k<4;k++) {
+	  m_transform[row*4+col] += store[row*4+k] * rotation[k*4+col];
+	}
+      }
+    }
+  }
 }
