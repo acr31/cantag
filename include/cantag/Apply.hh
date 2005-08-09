3cd3aa89cfd8373e12fe124d7a2fc3d0b23a58d6
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-08-09 13:46:54 +0000

    massive fiddling to try to get the validity stuff to work.  Defined a macro to print out the function name for debugging purposes.  Minor restructuring to allow the overloaded outer functions to call into explicitly named inner functions
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@688 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/cantag/Apply.hh
+++ include/cantag/Apply.hh
@@ -25,20 +25,41 @@
 #ifndef APPLY_GUARD
 #define APPLY_GUARD
 
+#include <vector>
+
 #include <cantag/ComposeEntity.hh>
 #include <cantag/EntityTree.hh>
 
+//#define PRINTFUNCTION() std::cout << __PRETTY_FUNCTION__ << std::endl
+#define PRINTFUNCTION()
 namespace Cantag {
 
   namespace Internal {
+
+    template<class Tree, class Arg,class Alg> struct ApplyAlternation {
+      static inline bool Choose(Tree& arg,Alg& alg) {
+	PRINTFUNCTION();
+	return _Apply_ComposedEntity(*(arg.GetNode()),alg);
+      }
+    };
+
+    template<class Tree, class Arg,class Alg> struct ApplyAlternation<Tree,TreeNode<Arg>,Alg > {
+      static inline bool Choose(Tree& arg, Alg& alg) {
+	PRINTFUNCTION();
+	return _Apply_ComposedEntityTree(arg,alg);
+      }
+    };
+
+
     /**
      * Apply the algorithm to this tree node and then recursively to the
      * children. Returns true if any of the function applications return
      * true
      */
     template<class Tree, class Algorithm> bool _Apply_Tree(Tree& tree, Algorithm& algorithm) {
-      bool result = Apply(*(tree.GetNode()),algorithm);
-      for(typename std::list<Tree*>::iterator i = tree.GetChildren().begin(); i != tree.GetChildren().end(); ++i) {
+      PRINTFUNCTION();
+      bool result = ApplyAlternation<Tree,typename Algorithm::ResultType,Algorithm>::Choose(tree,algorithm);
+      for(typename std::vector<Tree*>::iterator i = tree.GetChildren().begin(); i != tree.GetChildren().end(); ++i) {
 	result |= _Apply_Tree<Tree,Algorithm>(*(*i),algorithm);
       }    
       return result;
@@ -62,6 +83,7 @@ namespace Cantag {
 								    TreeNode<typename Algorithm::FunctionType::ResultType>& dest_parent, 
 								    TreeNode<typename Algorithm::FunctionType::ResultType>& dest_current, 
 								    const Algorithm& algorithm) {
+      PRINTFUNCTION();
       bool result = Apply(*(source.GetNode()), *(dest_current.GetNode()),algorithm);
       TreeNode<typename Algorithm::FunctionType::ResultType>* parent;
       TreeNode<typename Algorithm::FunctionType::ResultType>* child;
@@ -74,7 +96,7 @@ namespace Cantag {
 	child = &dest_current;
       }
       bool child_result = false;
-      for(typename std::list<Tree<TreeType>*>::const_iterator i = source.GetChildren().begin(); 
+      for(typename std::vector<Tree<TreeType>*>::const_iterator i = source.GetChildren().begin(); 
 	  i != source.GetChildren().end(); 
 	  ++i) {      
 	if (child_result) child = parent->AddChild();
@@ -84,18 +106,7 @@ namespace Cantag {
       return result;
     }
 
-    /*    template<class Algorithm, class List> bool _Apply_Compose_Tree(const Tree<ComposedEntity<List> >& source, 
-								   TreeNode<typename Algorithm::FunctionType::ResultType>& dest_parent, 
-								   TreeNode<typename Algorithm::FunctionType::ResultType>& dest_current, 
-								   const Algorithm& algorithm) {
-      if (source.IsPipelineValid()) {
-	typename Internal::ApplyHelperOuter<ComposedEntity<List> >::template ApplyHelper<Algorithm,typename Algorithm::FunctionType> a;
-	if (Position<typename Algorithm::ResultType,List>::value > entity.m_progress) entity.m_progress = Position<typename Algorithm::ResultType,List>::value;
-	if (a(algorithm,source.GetNode(),dest_current.GetNode())) return true;
-      }
-      return false; 
-    }
-    */
+    
     /**
      * Apply this 0-ary function to the entity.  0-ary functions are
      * deemed to be in-place mutators and so the result must be valid in
@@ -103,6 +114,7 @@ namespace Cantag {
      * then set according to the result of the algorithm
      */
     template<class Algorithm> bool _Apply_Fn0(typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+      PRINTFUNCTION();
       bool r = false;
       if (result.IsValid()) {
 	r = algorithm(result);
@@ -116,6 +128,7 @@ namespace Cantag {
      * result in result (setting its validity as returned by the function
      */
     template<class Algorithm> inline bool _Apply_Fn1(const typename Algorithm::FunctionType::SourceType1& arg1, typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+      PRINTFUNCTION();
       bool r = false;
       if (arg1.IsValid()) {
 	r = algorithm(arg1,result);
@@ -129,6 +142,7 @@ namespace Cantag {
      * and store the result in result and set its validity
      */
     template<class Algorithm> bool _Apply_Fn2(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, typename Algorithm::FunctionType::ResultType& result, const Algorithm& algorithm) {
+      PRINTFUNCTION();
       bool r = false;
       if (arg1.IsValid() && arg2.IsValid()) {
 	r = algorithm(arg1,arg2,result);
@@ -141,6 +155,7 @@ namespace Cantag {
      * Apply this 3-ary function to the arguments 
      */
     template<class Algorithm> bool _Apply_Fn3(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, const typename Algorithm::FunctionType::SourceType3& arg3, typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+      PRINTFUNCTION();
       bool r = false;
       if (arg1.IsValid() && arg2.IsValid() && arg3.IsValid()) {
 	r = algorithm(arg1,arg2,arg3,result);
@@ -158,6 +173,12 @@ namespace Cantag {
       class ApplyHelper<Algorithm,Function0<ReturnType> > {
       public:
 	inline bool operator()(Algorithm& algorithm, const ComposedEntity& me, ReturnType& result) const {
+	  PRINTFUNCTION();
+	  return _Apply_Fn0<Algorithm>(result,algorithm);
+	}
+
+	inline bool operator()(Algorithm& algorithm, const Tree<ComposedEntity>& me, ReturnType& result) const {
+	  PRINTFUNCTION();
 	  return _Apply_Fn0<Algorithm>(result,algorithm);
 	}
       };
@@ -166,6 +187,7 @@ namespace Cantag {
       class ApplyHelper<Algorithm,Function1<SourceType1,ReturnType> > {
       public:
 	inline bool operator()(Algorithm& algorithm, const ComposedEntity& me, ReturnType& result) const {
+	  PRINTFUNCTION();
 	  return _Apply_Fn1<Algorithm>(me,result,algorithm);
 	}
       };
@@ -174,6 +196,7 @@ namespace Cantag {
       class ApplyHelper<Algorithm,Function2<SourceType1,SourceType2,ReturnType> > {
       public:
 	inline bool operator()(Algorithm& algorithm, const ComposedEntity& me, ReturnType& result) const {
+	  PRINTFUNCTION();
 	  return _Apply_Fn2<Algorithm>(me,me,result,algorithm);
 	}
       };
@@ -182,91 +205,129 @@ namespace Cantag {
       class ApplyHelper<Algorithm,Function3<SourceType1,SourceType2,SourceType3, ReturnType> > {
       public:
 	inline bool operator()(Algorithm& algorithm, const ComposedEntity& me, ReturnType& result) const {
+	  PRINTFUNCTION();
 	  return _Apply_Fn3<Algorithm>(me,me,me,result,algorithm);
 	}
       };
     };
+
+    /**
+     * Use this algorithm to transform the data.  The algorithm will
+     * only be run if the pipeline is valid for this item - i.e the
+     * highest completed stage of the pipeline has valid data
+     *
+     * We then call the general apply function which will only call the
+     * function if all the source types have valid data
+     * 
+     * If the algorithm returns true then the output stage data will
+     * have been marked as valid and the highest stage index increased
+     * to the index of the output stage (if it is not bigger than it
+     * already)
+     */
+    template<class List, class Algorithm> bool _Apply_ComposedEntity(ComposedEntity<List>& entity, Algorithm& algorithm) {    
+      PRINTFUNCTION();
+      if (entity.IsPipelineValid()) {
+	typename Internal::ApplyHelperOuter<ComposedEntity<List> >::template ApplyHelper<Algorithm,typename Algorithm::FunctionType> a;
+	if (Position<typename Algorithm::ResultType,List>::value > entity.GetProgress()) {
+	  entity.SetProgress(Position<typename Algorithm::ResultType,List>::value);
+	}
+	bool result = a(algorithm,entity,entity);
+	entity.SetValid(result);
+	return result;
+      }
+      return false;
+    }
+
+    template<class List, class Algorithm> bool _Apply_ComposedEntityTree(Tree<ComposedEntity<List> >& entity, Algorithm& algorithm) {
+      PRINTFUNCTION();
+      if (entity.IsPipelineValid()) {
+	typename Internal::ApplyHelperOuter<ComposedEntity<List> >::template ApplyHelper<Algorithm,typename Algorithm::FunctionType> a;
+	if (Position<typename Algorithm::ResultType,List>::value > entity.GetProgress()) {
+	  entity.SetProgress(Position<typename Algorithm::ResultType,List>::value);
+	}
+	bool result = a(algorithm,entity,entity);
+	return result;
+      }
+      return false;
+    }
   }
 
-  template<class C, class Algorithm> bool ApplyTree(Tree<C>& tree, Algorithm& algorithm) {
+
+  template<class C, class Algorithm> inline bool ApplyTree(Tree<C>& tree, Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Tree<Tree<C>,Algorithm>(tree,algorithm);
   }
-  template<class C, class Algorithm> bool ApplyTree(const Tree<C>& tree, Algorithm& algorithm) {
+  template<class C, class Algorithm> inline bool ApplyTree(const Tree<C>& tree, Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Tree<const Tree<C>,Algorithm>(tree,algorithm);
   }
-  template<class C, class Algorithm> bool ApplyTree(Tree<C>& tree, const Algorithm& algorithm) {
+  template<class C, class Algorithm> inline bool ApplyTree(Tree<C>& tree, const Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Tree<Tree<C>,const Algorithm>(tree,algorithm);
   }
-  template<class C, class Algorithm> bool ApplyTree(const Tree<C>& tree, const Algorithm& algorithm) {
+  template<class C, class Algorithm> inline bool ApplyTree(const Tree<C>& tree, const Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Tree<const Tree<C>,const Algorithm>(tree,algorithm);
   }
 
-  template<class Algorithm> bool ApplyTree(const Tree<typename Algorithm::FunctionType::SourceType1>& source, 
+  template<class Algorithm> inline bool ApplyTree(const Tree<typename Algorithm::FunctionType::SourceType1>& source, 
 					   TreeNode<typename Algorithm::FunctionType::ResultType>& dest, 
 					   const Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Tree_Tree(source, dest,dest,algorithm);
   }
 
-  /**
-   * Use this algorithm to transform the data.  The algorithm will
-   * only be run if the pipeline is valid for this item - i.e the
-   * highest completed stage of the pipeline has valid data
-   *
-   * We then call the general apply function which will only call the
-   * function if all the source types have valid data
-   * 
-   * If the algorithm returns true then the output stage data will
-   * have been marked as valid and the highest stage index increased
-   * to the index of the output stage (if it is not bigger than it
-   * already)
-   */
-  template<class List, class Algorithm> bool Apply(ComposedEntity<List>& entity, Algorithm& algorithm) {    
-    if (entity.IsPipelineValid()) {
-      typename Internal::ApplyHelperOuter<ComposedEntity<List> >::template ApplyHelper<Algorithm,typename Algorithm::FunctionType> a;
-      if (Position<typename Algorithm::ResultType,List>::value > entity.m_progress) {
-	entity.m_progress = Position<typename Algorithm::ResultType,List>::value;
-      }
-      if (a(algorithm,entity,entity)) return true;
-    }
-    return false;
+  template<class List, class Algorithm> inline bool Apply(ComposedEntity<List>& entity, Algorithm& algorithm) {    
+    PRINTFUNCTION();
+    return Internal::_Apply_ComposedEntity(entity,algorithm);
   }
 
+  
   template<class Algorithm> inline bool Apply(typename Algorithm::FunctionType::ResultType& result, const Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn0<const Algorithm>(result,algorithm);
   }
   template<class Algorithm> inline bool Apply(typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn0<Algorithm>(result,algorithm);
   }
 
   template<class Algorithm> inline bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, typename Algorithm::FunctionType::ResultType& result, const Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn1<const Algorithm>(arg1,result,algorithm);
   }
 
   template<class Algorithm> inline bool Apply(TreeNode <typename Algorithm::FunctionType::SourceType1>& tree, typename Algorithm::FunctionType::ResultType& dest, Algorithm& algorithm) {
-      bool result = Apply(*(tree.GetNode()),dest,algorithm);
-      for(typename std::list<TreeNode <typename Algorithm::FunctionType::SourceType1>*>::iterator i = tree.GetChildren().begin(); i != tree.GetChildren().end(); ++i) {
-	result |= Apply<Algorithm>(*(*i),dest,algorithm);
-      }    
-      return result;
+    PRINTFUNCTION();
+    bool result = Apply(*(tree.GetNode()),dest,algorithm);
+    for(typename std::vector<TreeNode <typename Algorithm::FunctionType::SourceType1>*>::iterator i = tree.GetChildren().begin(); i != tree.GetChildren().end(); ++i) {
+      result |= Apply<Algorithm>(*(*i),dest,algorithm);
+    }    
+    return result;
   }
 
   template<class Algorithm> inline bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn1<Algorithm>(arg1,result,algorithm);    
   }
   
   template<class Algorithm> inline bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn2<Algorithm>(arg1,arg2,result,algorithm);
   }
 
   template<class Algorithm> inline bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, typename Algorithm::FunctionType::ResultType& result, const Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn2<const Algorithm>(arg1,arg2,result,algorithm);
   }
 
-  template<class Algorithm> bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, const typename Algorithm::FunctionType::SourceType3& arg3, typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+  template<class Algorithm> inline bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, const typename Algorithm::FunctionType::SourceType3& arg3, typename Algorithm::FunctionType::ResultType& result, Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn3<Algorithm>(arg1,arg2,arg3,result,algorithm);
   }
 
-  template<class Algorithm> bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, const typename Algorithm::FunctionType::SourceType3& arg3, typename Algorithm::FunctionType::ResultType& result, const Algorithm& algorithm) {
+  template<class Algorithm> inline bool Apply(const typename Algorithm::FunctionType::SourceType1& arg1, const typename Algorithm::FunctionType::SourceType2& arg2, const typename Algorithm::FunctionType::SourceType3& arg3, typename Algorithm::FunctionType::ResultType& result, const Algorithm& algorithm) {
+    PRINTFUNCTION();
     return Internal::_Apply_Fn3<const Algorithm>(arg1,arg2,arg3,result,algorithm);
   }
 };
