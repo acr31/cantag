5c798a975ee9f3e6337c43e139ba1d710fdb9212
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-08-09 13:33:21 +0000

    removed ShapeEntity to move to a separate file
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@671 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/cantag/entities/Entity.hh
+++ include/cantag/entities/Entity.hh
@@ -29,40 +29,24 @@
 
 #include <list>
 #include <vector>
-
+#include <iostream>
 namespace Cantag {
   class Entity {
   protected:
     bool m_valid;
     Entity() : m_valid(false) {};
   public:
-    void SetValid(bool valid) { m_valid = valid; }
+    void SetValid(bool valid) { m_valid = valid;}
     bool IsValid() const { return m_valid; }
   };
 
-
-
-  template<class Shape>
-  class ShapeEntity : public Entity {
-  public:
-    Shape* m_shapeDetails;
-    bool m_shapeFitted;
-  public:
-    ShapeEntity() : m_shapeDetails(NULL), m_shapeFitted(false) {};
-
-    ~ShapeEntity() {
-      if (m_shapeDetails) delete m_shapeDetails;
-    }
-
-  private:
-    ShapeEntity(const ShapeEntity<Shape>& copyme) : m_shapeDetails(copyme.m_shapeDetails ? new Shape(*copyme.m_shapeDetails) : NULL) {}
-  };
   
   class LocatedEntity : public Entity {
   protected:    
     float normal[3];
     float location[3];
     float angle;
+
   public:
     LocatedEntity() {};
     ~LocatedEntity() {};
