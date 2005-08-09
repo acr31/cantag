01a1de7f96c42f8a8411dc009b149a0216eaeefa
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-08-09 13:50:44 +0000

    implemented GetLocation and GetNormal
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@696 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/Transform.cc
+++ src/Transform.cc
@@ -89,7 +89,8 @@ namespace Cantag {
   }
 
   /**
-   * Rotate the tag by angle radians.
+   * Destructive rotation of the transform so the tag is rotated by
+   * angle radians.
    */
   void Transform::Rotate(float cos,float sin) {
     float rotation[16] = { cos, -sin,0,0,
@@ -103,12 +104,94 @@ namespace Cantag {
       m_transform[i] = 0;
     }
 
-    for(int row=0;row<4;row++) {
-      for(int col=0;col<4;col++) {
-	for(int k=0;k<4;k++) {
+    for(int row=0;row<4;++row) {
+      for(int col=0;col<4;++col) {
+	for(int k=0;k<4;++k) {
 	  m_transform[row*4+col] += store[row*4+k] * rotation[k*4+col];
 	}
       }
     }
+  }
+
+  /**
+   * Calculate the normal vector for the tag in this transform
+   */
+  void Transform::GetNormalVector(const Camera& cam, float normal[3]) {
+    // project (0,0,0) and (0,0,1).  Take the difference between them and normalize it
+    
+    // (0,0,0)
+    float proj0x = m_transform[3];
+    float proj0y = m_transform[7];
+    float proj0z = m_transform[11];
+    float proj0h = m_transform[15];
+    cam.CameraToWorld(proj0x/proj0h,proj0y/proj0h,proj0z/proj0h,&proj0x,&proj0y,&proj0z);
+    
+    // (0,0,1)
+    float proj1x = m_transform[2] + m_transform[3];
+    float proj1y = m_transform[6] + m_transform[7];
+    float proj1z = m_transform[10] + m_transform[11];
+    float proj1h = m_transform[14] + m_transform[15];
+    cam.CameraToWorld(proj1x/proj1h,proj1y/proj1h,proj1z/proj1h,&proj1x,&proj1y,&proj1z);
+
+    // (1,0,0)
+    float proj2x = m_transform[0] + m_transform[3];
+    float proj2y = m_transform[4] + m_transform[7];
+    float proj2z = m_transform[8] + m_transform[11];
+    float proj2h = m_transform[12] + m_transform[15];
+    cam.CameraToWorld(proj2x/proj2h,proj2y/proj2h,proj2z/proj2h,&proj2x,&proj2y,&proj2z);
+
+    // (0,1,0)
+    float proj3x = m_transform[1] + m_transform[3];
+    float proj3y = m_transform[5] + m_transform[7];
+    float proj3z = m_transform[9] + m_transform[11];
+    float proj3h = m_transform[13] + m_transform[15];
+    cam.CameraToWorld(proj3x/proj3h,proj3y/proj3h,proj3z/proj3h,&proj3x,&proj3y,&proj3z);
+
+
+    float v1x = proj2x - proj0x;
+    float v1y = proj2y - proj0y;
+    float v1z = proj2z - proj0z;
+
+    float v2x = proj3x - proj0x;
+    float v2y = proj3y - proj0y;
+    float v2z = proj3z - proj0z;
+
+
+    /*  
+	normal[0] = proj1x/proj1h - proj0x/proj0h;
+	normal[1] = proj1y/proj1h - proj0y/proj0h;
+	normal[2] = proj1z/proj1h - proj0z/proj0h;
+    */
+  
+  
+    // normal vector is the cross product
+    normal[0] = v1y * v2z - v1z * v2y;
+    normal[1] = v1z * v2x - v1x * v2z;
+    normal[2] = v1x * v2y - v1y * v2x;
+
+    float modulus = -sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2]);
+  
+    normal[0]/=modulus;
+    normal[1]/=modulus;
+    normal[2]/=modulus;
+
+#ifdef APPLY_TRANSFORM_DEBUG
+    PROGRESS("Found normal vector ("<<normal[0]<<","<<normal[1]<<","<<normal[2]<<")");
+#endif
+
+  }
+  
+  /**
+   * Calculate the origin for this tag.
+   *
+   * \todo call cameratoworld as in GetNormal
+   */
+  void Transform::GetLocation(float location[3], float tag_size) {
+    location[0] = tag_size*m_transform[3]/m_transform[15];
+    location[1] = tag_size*m_transform[7]/m_transform[15];
+    location[2] = tag_size*m_transform[11]/m_transform[15];
+#ifdef APPLY_TRANSFORM_DEBUG
+    PROGRESS("Found location ("<<location[0]<<","<<location[1]<<","<<location[2]<<")");
+#endif
   }
 }
