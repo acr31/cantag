1cbc18aa2cd657b2c87b48c83e0bec0aca7d3bd6
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-08-11 10:31:13 +0000

    changed Function specification to include explict argument and return types
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@703 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/cantag/Function.hh
+++ include/cantag/Function.hh
@@ -26,40 +26,20 @@
 #define ALGORITHM_GUARD
 
 #include <cantag/Config.hh>
+#include <cantag/TemplateUtils.hh>
 
 namespace Cantag {
 
-  template<class Result>
-  struct Function0 {
-    typedef Function0<Result> FunctionType;
-    typedef Result ResultType;
+  template<class ArgList, class ResultList>
+  struct Function {
+    typedef ArgList Arguments;
+    typedef ResultList Results;
+    typedef Function<ArgList,ResultList> FunctionType;
+    enum {
+      ArgumentArity = Length<ArgList>::value,
+      ReturnArity = Length<ResultList>::value
+    };
   };
-  
-  template<class Arg1, class Result>
-  struct Function1 {
-    typedef Function1<Arg1,Result> FunctionType;
-    typedef Arg1 SourceType1;
-    typedef Result ResultType;
-  };
-  
-  template<class Arg1, class Arg2, class Result>
-  struct Function2 {
-    typedef Function2<Arg1,Arg2,Result> FunctionType;
-    typedef Arg1 SourceType1;
-    typedef Arg2 SourceType2;
-    typedef Result ResultType;
-  };
-  
-  template<class Arg1, class Arg2, class Arg3, class Result>
-  struct Function3 {
-    typedef Function3<Arg1,Arg2,Arg3,Result> FunctionType;
-    typedef Arg1 SourceType1;
-    typedef Arg2 SourceType2;
-    typedef Arg3 SourceType3;
-    typedef Result ResultType;
-  };
-
-  
 
 }
 #endif//ALGORITHM_GUARD
