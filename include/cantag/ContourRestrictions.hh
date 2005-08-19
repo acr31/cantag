6d2c46393ee8c09368db4c3ecff0df7c38afa9b1
arb33 <arb33@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-08-19 13:52:31 +0000

    Added namespace
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@742 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/cantag/ContourRestrictions.hh
+++ include/cantag/ContourRestrictions.hh
@@ -25,29 +25,30 @@
 #ifndef CONTOUR_RESTRICTIONS_GUARD
 #define CONTOUR_RESTRICTIONS_GUARD
 
-class ContourRestrictions {
-private:
-  int m_minContourLength;
-  int m_minContourWidth;
-  int m_minContourHeight;    
-
-public:
-
-  ContourRestrictions(int minlength, int minwidth, int minheight) : m_minContourLength(minlength),m_minContourWidth(minwidth),m_minContourHeight(minheight) {}
-  
-  inline int GetMinContourLength() const { return m_minContourLength; }
-  inline int GetMinContourWidth() const { return m_minContourWidth; }
-  inline int GetMinContourHeight() const { return m_minContourHeight; }
-
-  inline void SetContourRestrictions(int minlength, int minwidth, int minheight) {
-    m_minContourLength = minlength;
-    m_minContourWidth = minwidth;
-    m_minContourHeight = minheight;
-  }
-
-  inline bool CheckDimensions(int length, int width, int height) const {
-    return length > m_minContourLength && width > m_minContourWidth && height > m_minContourHeight;
-  }
-};
-
+namespace Cantag {
+  class ContourRestrictions {
+  private:
+    int m_minContourLength;
+    int m_minContourWidth;
+    int m_minContourHeight;    
+    
+  public:
+    
+    ContourRestrictions(int minlength, int minwidth, int minheight) : m_minContourLength(minlength),m_minContourWidth(minwidth),m_minContourHeight(minheight) {}
+    
+    inline int GetMinContourLength() const { return m_minContourLength; }
+    inline int GetMinContourWidth() const { return m_minContourWidth; }
+    inline int GetMinContourHeight() const { return m_minContourHeight; }
+    
+    inline void SetContourRestrictions(int minlength, int minwidth, int minheight) {
+      m_minContourLength = minlength;
+      m_minContourWidth = minwidth;
+      m_minContourHeight = minheight;
+    }
+    
+    inline bool CheckDimensions(int length, int width, int height) const {
+      return length > m_minContourLength && width > m_minContourWidth && height > m_minContourHeight;
+    }
+  };
+}
 #endif//CONTOUR_RESTRICTIONS_GUARD
