aa96a55191d4a33013d4adf5ce486484988a80d0
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-08-30 17:40:40 +0000

    paginated constructor
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@750 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/QuadTangle.cc
+++ src/QuadTangle.cc
@@ -47,7 +47,10 @@ namespace Cantag {
 
   QuadTangle::QuadTangle() {}
 
-  QuadTangle::QuadTangle(float x0, float y0,float x1, float y1,float x2, float y2,float x3, float y3) : m_x0(x0), m_y0(y0),m_x1(x1), m_y1(y1),m_x2(x2), m_y2(y2),m_x3(x3), m_y3(y3) {
+  QuadTangle::QuadTangle(float x0, float y0,
+			 float x1, float y1,
+			 float x2, float y2,
+			 float x3, float y3) : m_x0(x0), m_y0(y0),m_x1(x1), m_y1(y1),m_x2(x2), m_y2(y2),m_x3(x3), m_y3(y3) {
     compute_central_point();
     sort_points();
   }
