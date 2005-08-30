a860c26e9adbddb980e3fecf225f604826ba90f0
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-08-30 17:45:49 +0000

    minor fiddling with the heuristics
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@755 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/cantag/algorithms/TransformSelectEllipse.hh
+++ include/cantag/algorithms/TransformSelectEllipse.hh
@@ -48,12 +48,18 @@ namespace Cantag {
   template<int RING_COUNT,int SECTOR_COUNT,int READ_COUNT> bool TransformSelectEllipseObj<RING_COUNT,SECTOR_COUNT,READ_COUNT>::operator()(TreeNode<TransformEntity>& node) const {
     // need a Transform entity
     TransformEntity& treenode = *(node.GetNode());
+    bool allfalse = true;
     for(std::list<Transform*>::iterator i = treenode.GetTransforms().begin(); i != treenode.GetTransforms().end(); ++i) {
       if ((*i)->GetConfidence() > 0.f) {
 	float normal[3];
 	(*i)->GetNormalVector(m_camera,normal);
 	float location[3];
 	(*i)->GetLocation(location,m_tagspec.GetBullseyeOuterEdge());
+
+	if (normal[3] > 0.f) { // if the normal faces away from the camera then this can't be right
+	  //	  (*i)->AccrueConfidence(0.f);
+	  //	  continue;
+	}
       
 	float maxConfidenceInChild = 0.f;
 	node.Reset();
@@ -67,41 +73,52 @@ namespace Cantag {
 		(*k)->GetNormalVector(m_camera,childnormal);
 		float childlocation[3];
 		(*k)->GetLocation(childlocation,m_tagspec.GetBullseyeInnerEdge());
-	      		
+		
+		if (childnormal[3] > 0.f) {// if the normal faces away from the camera then this can't be right
+		  //continue;
+		}
 		float angleconfidence = normalVectorConfidence(normal,childnormal);
 		float locationconfidence = locationConfidence(location,childlocation);
 		float confidence = angleconfidence * locationconfidence;
+		//		std::cout << angleconfidence << " " << locationconfidence << "(" << maxConfidenceInChild << "," << confidence << ")" << std::endl;
 		if (confidence > maxConfidenceInChild) maxConfidenceInChild = confidence;
 	      }
 	    }
 	  }
 	}
-      
+	
+	if (maxConfidenceInChild != 0.f) allfalse = false;
+
 	(*i)->AccrueConfidence(maxConfidenceInChild);
       }
     }
-    return true;
+    return !allfalse;
   }
 
   template<int RING_COUNT,int SECTOR_COUNT,int READ_COUNT> float TransformSelectEllipseObj<RING_COUNT,SECTOR_COUNT,READ_COUNT>::locationConfidence(float location1[3], float location2[3]) const {
 
-    float distance = 
-      (location1[0]-location2[0])*(location1[0]-location2[0])+
-      (location1[1]-location2[1])*(location1[1]-location2[1])+
-      (location1[2]-location2[2])*(location1[2]-location2[2]);
-    if (distance > 50.f) return 0.f;
-    else return 1.f-distance/50.f;
-
+    float distance = ((location1[0]-location2[0])*(location1[0]-location2[0])+
+		      (location1[1]-location2[1])*(location1[1]-location2[1])+
+		      (location1[2]-location2[2])*(location1[2]-location2[2]));
+    return 10000.f-distance;
+    //    distance *= distance;
+    //    std::cout << "distance " << distance << std::endl;
+    //    if (distance > 10.f) return 0.f;
+    //    else return 1.f-distance/10.f;
   }
 
   template<int RING_COUNT,int SECTOR_COUNT,int READ_COUNT> float TransformSelectEllipseObj<RING_COUNT,SECTOR_COUNT,READ_COUNT>::normalVectorConfidence(float normal1[3], float normal2[3]) const {
   
-    float cosangle = normal1[0]*normal2[0]+normal1[1]*normal2[1]+normal1[2]*normal2[2];
-  
-    float angle = acos(cosangle);
-    //    if (angle > 0.17453293) return 0.f; else return 1.f;
-    return angle / 2.f / M_PI;
+    //    float cosangle = normal1[0]*normal2[0]+normal1[1]*normal2[1]+normal1[2]*normal2[2];
 
+    //    if (cosangle > 1.f) cosangle = 1.f;
+    //    if (cosangle < -1.f) cosangle = -1.f;
+
+    //    float angle = acos(cosangle);
+    //    std::cout << "angle " << angle << std::endl;
+    return 1.f;
+    //    if (angle > M_PI/8.f) return 0.f; else return 1.f;
+    //return 1.f;
     // turn angle into probability
     // might want to accept a few degrees variance as not affecting the probability
     // also 90 degrees is impossible
