1f4fc90771a2ae47160a91634ad37b3621462323
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-09-06 13:38:34 +0000

    added regression fitting for quadtangles
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@769 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ src/algorithms/FitQuadTangleRegression.cc
@@ -0,0 +1,132 @@
+/*
+  Copyright (C) 2004 Andrew C. Rice
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Email: acr31@cam.ac.uk
+*/
+
+/**
+ * $Header$
+ */ 
+
+#include <cantag/algorithms/FitQuadTangleRegression.hh>
+
+namespace Cantag {
+
+  bool FitQuadTangleRegression::operator()(const ContourEntity& contour, ShapeEntity<QuadTangle>& shape) const {
+
+    const std::vector<float>& points = contour.GetPoints();
+    // Calculate the indexes of the current points
+    std::vector<int> indexes(4);
+    indexes[0] = shape.GetShape()->GetIndex0();
+    indexes[1] = shape.GetShape()->GetIndex1();
+    indexes[2] = shape.GetShape()->GetIndex2();
+    indexes[3] = shape.GetShape()->GetIndex3();
+    sort(indexes.begin(), indexes.end());
+    // Now we have estimates of the corner indexes within points So do
+    // some regression. We ignore the 4 points next to an estimated
+    // corner since these are less reliable indicators of the side
+
+    // Each line has equation y=mx+c
+    // OR x=c
+
+    float m[4]={0.0};
+    float c[4]={0.0};
+    bool  yeq[4]={1};
+    
+    for (int j=0; j<4; j++) {
+      float xsum=0.0;
+      float ysum = 0.0;
+      float xxsum=0.0;
+      float xysum=0.0;
+      int count=0;
+      
+      int start = indexes[j];
+      int end = indexes[(j+1)%4]%(points.size()/2);
+      
+      if (end < start) end+=points.size()/2;
+      
+      // Ignore the first and last 3 points
+      float lastx=points[(start+4)*2];
+      bool vertical=true;
+      for (int i=start+4; i<end-4; i++) {
+	int ii = i%(points.size()/2);
+	
+	float x = points[ii*2];
+	float y = points[ii*2+1];
+	
+	xsum+=x;
+	ysum+=y;
+	xxsum+=x*x;
+	xysum+=x*y;
+	count++;
+	if (x!=lastx) vertical=false;
+	lastx = x;
+      }
+      
+      if (vertical) {
+	c[j] = lastx;
+	yeq[j]=0;
+      }
+      else {
+	float numer = (xysum - xsum*ysum/(float)count);
+	float denom = (xxsum - xsum*xsum/(float)count);
+	m[j] = numer/denom;
+	c[j] = ysum/(float)count - m[j]*xsum/(float)count;
+	yeq[j]=1;
+      }
+    }
+    
+    // Now have four lines and we need the intersections
+    
+    float xres[4];
+    float yres[4];
+    
+    
+    
+    for (int j=0; j<4; j++) {
+      //    std::cout << "Y " << yeq[j] << " " << (j-1)%4 << std::endl;
+      int lastj = (j-1);
+      if (lastj==-1) lastj=3;
+      if (yeq[j] && yeq[lastj]) {
+	xres[j] = (c[lastj]-c[j])/(m[j]-m[lastj]);
+	yres[j] = m[j]*xres[j]+c[j];
+      }
+      else if (yeq[j] && !yeq[lastj]) {
+	xres[j] = c[lastj];
+	yres[j] = m[j]*xres[j] + c[j];
+      }
+      else if (!yeq[j] && yeq[lastj]) {
+	xres[j] = c[j];
+	yres[j] = m[lastj]*xres[j] + c[lastj];
+      }
+      else {
+	// so they don't intersect
+	return false;
+      }
+    }
+
+    shape.GetShape()->Set(xres[0],yres[0],
+			  xres[1],yres[1],
+			  xres[2],yres[2],
+			  xres[3],yres[3],
+			  -1,-1,-1,-1);
+
+    return true;
+
+  }
+
+}
