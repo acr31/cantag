e2af54f49ca7d235c7b8669457125ac416e103aa
rkh23 <rkh23@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-09-07 17:00:57 +0000

    A new symbol chunk coder that has a per block CRC of definable size
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@780 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ include/cantag/coders/CRC.hh
@@ -0,0 +1,338 @@
+#ifndef CRC_HH
+#define CRC_HH
+
+#include <bitset>
+#include <iostream>
+
+
+template <int CRC_SIZE, int PAYLOAD_SIZE>
+struct CRCPolynomial {
+  enum {
+    Value = CRCPolynomial<CRC_SIZE,PAYLOAD_SIZE-1>::Value
+  };
+};
+
+
+
+/**
+ * A SIMPLE implementation of a CRC
+ * No doubt it could be improved, but it works!
+ */
+template <int DATA_BITS, int CRC_BITS> class CRC {
+public:
+
+  std::bitset<CRC_BITS> GetCRC(std::bitset<DATA_BITS> &payload) {
+    // First of all get the polynomial for this setup
+    std::bitset<CRC_BITS+1> gen_poly = std::bitset<CRC_BITS+1>( CRCPolynomial<CRC_BITS,DATA_BITS>::Value );
+
+    std::bitset<CRC_BITS+1> numer(0);
+    std::bitset<CRC_BITS> result(0);
+    
+    for (int i=0; i<CRC_BITS+1; i++) {
+      if (payload[DATA_BITS-1-i]) numer.set(CRC_BITS-i,1);
+    }
+    
+    for (int it=0; it<(DATA_BITS+CRC_BITS-CRC_BITS); it++) {
+      
+      if (numer[CRC_BITS]) numer = numer ^ gen_poly;  
+      numer <<= 1;
+      int idx = DATA_BITS-1-(CRC_BITS+1)-it;
+      if (idx>=0 && payload[idx]) numer.set(0,1);
+    }
+    numer >>= 1;
+    
+    for (int i=0; i<CRC_BITS; i++) {
+      if (numer[i]) result.set(i,1);
+    }
+    
+    return result;
+  }
+}; 
+
+
+
+
+
+
+template<> struct CRCPolynomial<3,0> {
+	enum {
+		Value = 5
+	};
+};
+template<> struct CRCPolynomial<4,0> {
+	enum {
+		Value = 9
+	};
+};
+template<> struct CRCPolynomial<4,12> {
+	enum {
+		Value = 9
+	};
+};
+template<> struct CRCPolynomial<5,0> {
+	enum {
+		Value = 21
+	};
+};
+template<> struct CRCPolynomial<5,11> {
+	enum {
+		Value = 18
+	};
+};
+template<> struct CRCPolynomial<5,27> {
+	enum {
+		Value = 18
+	};
+};
+template<> struct CRCPolynomial<6,0> {
+	enum {
+		Value = 44
+	};
+};
+template<> struct CRCPolynomial<6,26> {
+	enum {
+		Value = 18
+	};
+};
+template<> struct CRCPolynomial<6,58> {
+	enum {
+		Value = 33
+	};
+};
+template<> struct CRCPolynomial<7,0> {
+	enum {
+		Value = 91
+	};
+};
+template<> struct CRCPolynomial<7,57> {
+	enum {
+		Value = 72
+	};
+};
+template<> struct CRCPolynomial<7,121> {
+	enum {
+		Value = 72
+	};
+};
+template<> struct CRCPolynomial<8,0> {
+	enum {
+		Value = 156
+	};
+};
+template<> struct CRCPolynomial<8,10> {
+	enum {
+		Value = 151
+	};
+};
+template<> struct CRCPolynomial<8,120> {
+	enum {
+		Value = 166
+	};
+};
+template<> struct CRCPolynomial<8,248> {
+	enum {
+		Value = 166
+	};
+};
+template<> struct CRCPolynomial<9,0> {
+	enum {
+		Value = 316
+	};
+};
+template<> struct CRCPolynomial<9,9> {
+	enum {
+		Value = 389
+	};
+};
+template<> struct CRCPolynomial<9,14> {
+	enum {
+		Value = 331
+	};
+};
+template<> struct CRCPolynomial<9,247> {
+	enum {
+		Value = 359
+	};
+};
+template<> struct CRCPolynomial<9,503> {
+	enum {
+		Value = 359
+	};
+};
+template<> struct CRCPolynomial<10,0> {
+	enum {
+		Value = 654
+	};
+};
+template<> struct CRCPolynomial<10,13> {
+	enum {
+		Value = 697
+	};
+};
+template<> struct CRCPolynomial<10,22> {
+	enum {
+		Value = 793
+	};
+};
+template<> struct CRCPolynomial<10,502> {
+	enum {
+		Value = 807
+	};
+};
+template<> struct CRCPolynomial<10,1014> {
+	enum {
+		Value = 807
+	};
+};
+template<> struct CRCPolynomial<11,0> {
+	enum {
+		Value = 1393
+	};
+};
+template<> struct CRCPolynomial<11,13> {
+	enum {
+		Value = 1330
+	};
+};
+template<> struct CRCPolynomial<11,23> {
+	enum {
+		Value = 1495
+	};
+};
+template<> struct CRCPolynomial<11,26> {
+	enum {
+		Value = 1411
+	};
+};
+template<> struct CRCPolynomial<11,1013> {
+	enum {
+		Value = 1613
+	};
+};
+template<> struct CRCPolynomial<11,2037> {
+	enum {
+		Value = 1613
+	};
+};
+template<> struct CRCPolynomial<12,0> {
+	enum {
+		Value = 2639
+	};
+};
+template<> struct CRCPolynomial<12,12> {
+	enum {
+		Value = 2881
+	};
+};
+template<> struct CRCPolynomial<12,28> {
+	enum {
+		Value = 2296
+	};
+};
+template<> struct CRCPolynomial<12,54> {
+	enum {
+		Value = 3079
+	};
+};
+template<> struct CRCPolynomial<12,2036> {
+	enum {
+		Value = 2933
+	};
+};
+template<> struct CRCPolynomial<13,0> {
+	enum {
+		Value = 4279
+	};
+};
+template<> struct CRCPolynomial<13,12> {
+	enum {
+		Value = 4773
+	};
+};
+template<> struct CRCPolynomial<13,13> {
+	enum {
+		Value = 6409
+	};
+};
+template<> struct CRCPolynomial<13,53> {
+	enum {
+		Value = 4138
+	};
+};
+template<> struct CRCPolynomial<14,0> {
+	enum {
+		Value = 9073
+	};
+};
+template<> struct CRCPolynomial<14,12> {
+	enum {
+		Value = 10409
+	};
+};
+template<> struct CRCPolynomial<14,14> {
+	enum {
+		Value = 14123
+	};
+};
+template<> struct CRCPolynomial<14,58> {
+	enum {
+		Value = 8493
+	};
+};
+template<> struct CRCPolynomial<14,113> {
+	enum {
+		Value = 8680
+	};
+};
+template<> struct CRCPolynomial<15,0> {
+	enum {
+		Value = 25355
+	};
+};
+template<> struct CRCPolynomial<15,13> {
+	enum {
+		Value = 23509
+	};
+};
+template<> struct CRCPolynomial<15,17> {
+	enum {
+		Value = 22330
+	};
+};
+template<> struct CRCPolynomial<15,115> {
+	enum {
+		Value = 27277
+	};
+};
+template<> struct CRCPolynomial<15,137> {
+	enum {
+		Value = 18806
+	};
+};
+template<> struct CRCPolynomial<16,0> {
+	enum {
+		Value = 36827
+	};
+};
+template<> struct CRCPolynomial<16,16> {
+	enum {
+		Value = 38539
+	};
+};
+template<> struct CRCPolynomial<16,20> {
+	enum {
+		Value = 51308
+	};
+};
+template<> struct CRCPolynomial<16,136> {
+	enum {
+		Value = 44186
+	};
+};
+template<> struct CRCPolynomial<16,242> {
+	enum {
+		Value = 47789
+	};
+};
+
+#endif
---
+++ include/cantag/coders/CRCSymbolChunkCoder.hh
@@ -0,0 +1,122 @@
+/**
+ * $Header: /filer/acr31/unix_home/cvsroot/src/total/include/total/coders/SymbolChunkCoder.hh,v 1.3 2005/05/24 07:44:21 rkh23 Exp $
+ */
+
+#ifndef CRC_SYMBOL_CHUNK_CODER
+#define CRC_SYMBOL_CHUNK_CODER
+
+#include <cantag/Config.hh>
+#include <cantag/coders/Coder.hh>
+#include <cantag/coders/CRC.hh>
+
+#include <iostream>
+
+#define CRC_SYMBOL_CHUNK_DEBUG
+
+namespace Cantag {
+  /**
+   * Encodes the data into a number of symbols each of GRANULARITY bits
+   * The first bit of each symbol is the orientation bit - 1 for
+   * the first symbol, CRC_BITS are used for a CRC and the rest
+   * (GRANULARITY-1-CRC_BITS) make up the data in the block
+   */
+  template<int BIT_COUNT, int CRC_BITS, int GRANULARITY>
+  class CRCSymbolChunkCoder : public virtual Coder<BIT_COUNT> {
+  public:
+    virtual bool IsErrorCorrecting() const;
+    virtual int GetSymbolSize() const;
+    virtual int GetHammingDistanceBits() const;
+    virtual int GetHammingDistanceSymbols() const;
+
+
+    virtual int DecodePayload(CyclicBitSet<BIT_COUNT>& data) const;
+  
+    virtual bool EncodePayload(CyclicBitSet<BIT_COUNT>& data) const;
+  };
+
+
+  template<int BIT_COUNT,int CRC_BITS, int GRANULARITY> bool CRCSymbolChunkCoder<BIT_COUNT,CRC_BITS,GRANULARITY>::IsErrorCorrecting() const { return false; }
+
+  template<int BIT_COUNT,int CRC_BITS, int GRANULARITY> int CRCSymbolChunkCoder<BIT_COUNT,CRC_BITS,GRANULARITY>::GetSymbolSize() const { return GRANULARITY; }
+
+  template<int BIT_COUNT,int CRC_BITS, int GRANULARITY> int CRCSymbolChunkCoder<BIT_COUNT,CRC_BITS,GRANULARITY>::GetHammingDistanceBits() const { return 2; }
+
+  template<int BIT_COUNT,int CRC_BITS, int GRANULARITY> int CRCSymbolChunkCoder<BIT_COUNT,CRC_BITS,GRANULARITY>::GetHammingDistanceSymbols() const { return 0; }
+
+
+  template<int BIT_COUNT, int CRC_BITS, int GRANULARITY> int CRCSymbolChunkCoder<BIT_COUNT,CRC_BITS,GRANULARITY>::DecodePayload(CyclicBitSet<BIT_COUNT>& data) const {
+
+    // Check the CRC on each block
+    for (int j=0; j<BIT_COUNT/GRANULARITY; j++) {
+      std::bitset<GRANULARITY-CRC_BITS> pl;
+      for (int i=0; i<GRANULARITY-CRC_BITS; i++) pl[i]=data[j*GRANULARITY+i];
+      CRC<GRANULARITY-CRC_BITS, CRC_BITS> crc;
+      std::bitset<CRC_BITS> c = crc.GetCRC(pl);
+      std::bitset<CRC_BITS> c_read;
+      for (int a=0; a<CRC_BITS; a++) {
+	c_read[a]=data[j*GRANULARITY+GRANULARITY-CRC_BITS+a];
+      }
+      //      std::cout << data << " " << pl << " " << c_read << " " << c << std::endl;
+      if (c_read!=c) return -1;
+    }
+
+    // rotate the code by increments of GRANULARITY until the first bit is a 1
+    int rotation = 0;
+    while(!data[0] && rotation < BIT_COUNT) {
+      data.RotateLeft(GRANULARITY);
+      rotation+=GRANULARITY;
+    }
+
+    if (data[0]) {
+      CyclicBitSet<BIT_COUNT> data_copy(data);
+      data.reset();
+      // Concatenate the payload
+      unsigned int pld=0;
+      for (int i=0; i<BIT_COUNT; i+=GRANULARITY) {
+	for (int j=0; j<GRANULARITY-1-CRC_BITS; j++) {
+	  data[pld++]=data_copy[i+j+1];
+	}
+      }
+      return rotation;
+    }
+    else {
+#ifdef CRC_SYMBOL_CHUNK_DEBUG
+      PROGRESS("Failed to find orientation start bit");
+#endif
+      return -1;
+    }
+  }
+
+  template<int BIT_COUNT,int CRC_BITS, int GRANULARITY> bool CRCSymbolChunkCoder<BIT_COUNT,CRC_BITS,GRANULARITY>::EncodePayload(CyclicBitSet<BIT_COUNT>& data) const {
+    CyclicBitSet<BIT_COUNT> data_copy(data); 
+    data.reset();
+
+    int payload_pointer = 0;
+    for(int i=0;i<BIT_COUNT/GRANULARITY * (GRANULARITY-1-CRC_BITS);i+=GRANULARITY-1-CRC_BITS) {
+
+      std::bitset<GRANULARITY-CRC_BITS> toenc(0);
+      int toenci=0;
+
+      // First the marker
+      data[payload_pointer++] = (i==0);  // encode a 1 if this is the first symbol
+      toenc[toenci++] = (i==0); 
+
+      // Now the data
+      for(int j=0;j<GRANULARITY-1-CRC_BITS;j++) {
+	data[payload_pointer++] = data_copy[i+j];
+	toenc[toenci++] = data_copy[i+j];
+      }
+
+      // Now the checksum per block
+      CRC<GRANULARITY-CRC_BITS, CRC_BITS> crc;
+      std::bitset<CRC_BITS> c = crc.GetCRC(toenc);
+
+      for (int k=0; k<CRC_BITS; k++) {
+	data[payload_pointer++] = c[k];
+      }
+    }
+    return true;
+  }
+
+}
+#endif//SYMBOL_CHUNK_CODER
