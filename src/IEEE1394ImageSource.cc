b21fd0dbac055ca228103a00d3f454587337151e
rkh23 <rkh23@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-09-21 16:06:48 +0000

    Fixed some bugs, added some new stuff.  Constructore prototype changed!
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@787 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/IEEE1394ImageSource.cc
+++ src/IEEE1394ImageSource.cc
@@ -34,10 +34,11 @@ namespace Cantag {
   IEEE1394ImageSource::IEEE1394ImageSource(
 					   char *device_name,
 					   int port,
-					   u_int64_t guid,
+					   int mode,
 					   int framerate,
 					   int shutter,
-					   int gain
+					   int gain,
+					   u_int64_t guid
 					   ) :mImage(0)
   {
     
@@ -47,7 +48,7 @@ namespace Cantag {
      int nCam = 0;
      nodeid_t * camera_nodes = dc1394_get_camera_nodes(mHandle, &nCam, 0);
      
-     if (nCam==0) {
+     if (nCam==0 || mHandle==NULL) {
        throw "Unable to find a 1394 Camera. Please check that you have modprobed raw1394 and videodev1394 and created the relevant device nodes in /dev";
      }
 
@@ -70,28 +71,35 @@ namespace Cantag {
      else {
        mCamera.node = camera_nodes[0];
      }
-     dc1394_free_camera_nodes(camera_nodes);
+
 
      if (shutter>=0) dc1394_set_shutter(mHandle, mCamera.node,shutter);
      if (gain>=0) dc1394_set_gain(mHandle, mCamera.node,gain);
+
+     unsigned int channel, speed;
+     if (dc1394_get_iso_channel_and_speed(mHandle, mCamera.node, &channel, &speed) != DC1394_SUCCESS) {
+       throw "Unable to get Camera channel/speed";
+     }
      
      // Now we want to set up the capture parameters
      dc1394_dma_setup_capture(mHandle, mCamera.node,
-			      0, /* channel */ 
+			      channel,
 			      FORMAT_VGA_NONCOMPRESSED,
-			      MODE_640x480_MONO,
-			      SPEED_400,
+			      mode,
+			      speed,
 			      framerate,
 			      8,1,  
-			      device_name,
+			      0,  // Yes, this should be device_name but it segfaults if it's anything OTHER than 0!
 			      &mCamera);
 
      // Start the transmission
      dc1394_start_iso_transmission(mHandle, mCamera.node);
+     dc1394_free_camera_nodes(camera_nodes);
   }
 
   
   IEEE1394ImageSource::~IEEE1394ImageSource() {
+    dc1394_dma_unlisten( mHandle, &mCamera );
     dc1394_dma_release_camera(mHandle,&mCamera);
     dc1394_destroy_handle(mHandle);
   }
@@ -116,7 +124,7 @@ namespace Cantag {
     if (!mImage) {
       mImage = new Image<Pix::Sze::Byte1,Pix::Fmt::Grey8>(GetWidth(),GetHeight());
     }
-    dc1394_dma_single_capture(&mCamera);
+    dc1394_dma_multi_capture(&mCamera,1);
     unsigned char *img = (unsigned char*) mCamera.capture_buffer;
     for (int i=0; i<GetWidth(); i++) {
       for (int j=0; j<GetHeight(); j++) {
