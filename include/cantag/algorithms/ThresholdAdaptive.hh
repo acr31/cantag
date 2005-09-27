9b1217ae431e5e0164bc46334c2f7912f203fef7
rkh23 <rkh23@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-09-27 15:00:50 +0000

    Templated the adaptive threshold
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@792 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- include/cantag/algorithms/ThresholdAdaptive.hh
+++ include/cantag/algorithms/ThresholdAdaptive.hh
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2004 Andrew C. Rice
+  Copyright (C) 2004 Andrew C. Rice, Robert K. Harle
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
@@ -47,15 +47,72 @@ namespace Cantag {
    * average. the window used is now 2^window_size
    *
    */
-  class ThresholdAdaptive : public Function<TypeList<Image<Pix::Sze::Byte1,Pix::Fmt::Grey8> >, // needed because the preprocesser can't parse the call to TL1(Image<Pix::Sze::Byte1,Pix::Fmt::Grey8>)
-		    TL1(MonochromeImage)> {
+
+  template<Pix::Sze::Bpp size,Pix::Fmt::Layout layout> class ThresholdAdaptive
+    : public Function<TypeList<Image<size,layout> >, // needed because the preprocessor can't parse the call to TL1(Image<size,layout>)
+    TL1(MonochromeImage)> {
   private:
     int m_window_size;
     int m_offset;
+  
   public:
     ThresholdAdaptive(int window_size, int offset);
-    bool operator()(const Image<Pix::Sze::Byte1,Pix::Fmt::Grey8>& source, MonochromeImage& dest) const;
+    bool operator()(const Image<size,layout>& source, MonochromeImage& dest) const;
   };
+
+
+  template<Pix::Sze::Bpp size,Pix::Fmt::Layout layout> ThresholdAdaptive<size,layout>::ThresholdAdaptive(int window_size, 
+														 int offset)
+    : m_window_size(window_size), m_offset(offset) {}
+
+
+  template<Pix::Sze::Bpp size,Pix::Fmt::Layout layout> 
+  bool ThresholdAdaptive<size,layout>::operator()(const Image<size,layout>& image, MonochromeImage& dest) const {
+     int moving_average = 127;
+    const int image_width = image.GetWidth();
+    const int image_height = image.GetHeight();
+    const int useoffset = 255-m_offset;
+    
+    int previous_line[image_width];
+    for(int i=0;i<image_width;++i) { previous_line[i] = 127; }
+    
+    for(int i=0;i<image_height-1;) { // use height-1 so we dont overrun the image if its height is an odd number
+      const PixRow<layout> row = image.GetRow(i);
+      typename PixRow<layout>::const_iterator x = row.begin();
+      for (int j=0; j<image_width;++j) {
+	const int pixel = (*x).intensity(); ++x;
+
+	moving_average = pixel + moving_average - (moving_average >> m_window_size);
+	int current_thresh = (moving_average + previous_line[j])>>1;
+	previous_line[j] = moving_average;      
+	dest.SetPixel(j,i, (pixel << m_window_size+8) < (current_thresh * useoffset) );
+      }
+
+      ++i;
+      x=row.end()-1; 
+      for(int j=image_width-1;j>=0;--j) {
+	const int pixel = (*x).intensity(); --x;
+
+	moving_average = pixel + moving_average - (moving_average >> m_window_size);
+	int current_thresh = (moving_average + previous_line[j])>>1;
+	previous_line[j] = moving_average;
+	dest.SetPixel(j,i, (pixel << m_window_size+8) < (current_thresh * useoffset) );
+      }
+      ++i;
+    }  
+
+    for(int i=0;i<image_height;++i) {
+      dest.SetPixel(0,i,false);
+      dest.SetPixel(image_width-1,i,false);
+      if (i == 0 || i == image_height-1) {
+	for(int j=0;j<image_width-1;++j) {
+	  dest.SetPixel(j,i,false);
+	}
+      }
+    }
+    return true;
+  }
+
 }
 
 #endif//THRESHOLD_ADAPTIVE_GUARD
