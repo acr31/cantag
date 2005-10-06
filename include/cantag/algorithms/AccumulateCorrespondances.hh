e918a857a8dc751ef92aeee34c727b7d28e07192
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-10-06 15:06:44 +0000

    algorithm for building the correspondance set on an entity tree - i.e. the relation between physical locations from the world model and those found in the image
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@817 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ include/cantag/algorithms/AccumulateCorrespondances.hh
@@ -0,0 +1,55 @@
+/*
+  Copyright (C) 2005 Andrew C. Rice
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Email: acr31@cam.ac.uk
+*/
+
+/**
+ * $Header$
+ */
+
+#ifndef ACCUMULATECORRESPONDANCE_GUARD
+#define ACCUMULATECORRESPONDANCE_GUARD
+
+#include <set>
+
+#include <cantag/Config.hh>
+#include <cantag/Function.hh>
+#include <cantag/entities/TransformEntity.hh>
+#include <cantag/entities/DecodeEntity.hh>
+#include <cantag/Correspondances.hh>
+#include <cantag/TagDictionary.hh>
+
+namespace Cantag {
+
+  template<int PAYLOAD_SIZE>
+  class AccumulateCorrespondances : public Function<TL1(TransformEntity),TL1(DecodeEntity<PAYLOAD_SIZE>)> {
+  private:
+    Correspondances<LocationElement>& m_corr;
+    const TagDictionary<PAYLOAD_SIZE,TL1(LocationElement)>& m_dictionary;
+  public:
+    AccumulateCorrespondances(Correspondances<LocationElement>& corr, const TagDictionary<PAYLOAD_SIZE,TL1(LocationElement)>& dictionary) : m_corr(corr), m_dictionary(dictionary) {}
+    bool operator()(const TransformEntity& trans, DecodeEntity<PAYLOAD_SIZE>& decode) const;
+  };
+
+  template<int PAYLOAD_SIZE> bool AccumulateCorrespondances<PAYLOAD_SIZE>::operator()(const TransformEntity& trans, DecodeEntity<PAYLOAD_SIZE>& decode) const {
+    const LocationElement* lookup = m_dictionary.GetInformation((*(decode.GetPayloads().begin()))->payload);
+    if (lookup)
+      m_corr.Put(trans,lookup);
+  }
+}
+#endif//ACCUMULATECORRESPONDANCE_GUARD
