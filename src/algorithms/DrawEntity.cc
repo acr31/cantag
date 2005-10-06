d99d7aaf5a52eae25f1578273501409664406fa4
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-10-06 14:59:09 +0000

    more drawing algorithms
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@809 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/algorithms/DrawEntity.cc
+++ src/algorithms/DrawEntity.cc
@@ -26,6 +26,24 @@
 
 namespace Cantag {
 
+  bool DrawEntityImage::operator()(Image<Pix::Sze::Byte1,Pix::Fmt::Grey8>& image) const {    
+    for(int i=m_roi.minx;i<m_roi.maxx;++i) {
+      for(int j=m_roi.miny;j<m_roi.maxy;++j) {
+	m_image.DrawPixel(m_roi.ScaleX(i,m_image.GetWidth()),m_roi.ScaleY(j,m_image.GetHeight()),image.Sample(i,j));
+      }
+    }
+    return true;
+  }
+
+  bool DrawEntityMonochrome::operator()(MonochromeImage& monochrome) const {
+    for(int i=0;i<monochrome.GetWidth();++i) {
+      for(int j=0;j<monochrome.GetHeight();++j) {
+	m_image.DrawPixel(m_roi.ScaleX(i,m_image.GetWidth()),m_roi.ScaleY(j,m_image.GetHeight()),monochrome.GetPixel(i,j) ? 255 : 0);
+      }
+    }
+    return true;
+  }
+
   bool DrawEntityContour::operator()(ContourEntity& contour) const {
     for(std::vector<float>::const_iterator i = contour.GetPoints().begin();
 	i != contour.GetPoints().end();
@@ -33,7 +51,7 @@ namespace Cantag {
       const float x = *i;
       ++i;
       const float y = *i;
-      m_image.DrawPixel(x,y,0);
+      m_image.DrawPixel(m_roi.ScaleX(x,m_image.GetWidth()),m_roi.ScaleY(y,m_image.GetHeight()),0);
     }    
     return true;
   }
@@ -45,10 +63,10 @@ namespace Cantag {
 		   1,-1};
     transform.GetPreferredTransform()->Apply(pts,4);
     m_camera.NPCFToImage(pts,4);
-    m_image.DrawQuadTangle((int)pts[0],(int)pts[1],
-			   (int)pts[2],(int)pts[3],
-			   (int)pts[4],(int)pts[5],
-			   (int)pts[6],(int)pts[7],
+    m_image.DrawQuadTangle((int)m_roi.ScaleX(pts[0],m_image.GetWidth()),(int)m_roi.ScaleY(pts[1],m_image.GetHeight()),
+			   (int)m_roi.ScaleX(pts[2],m_image.GetWidth()),(int)m_roi.ScaleY(pts[3],m_image.GetHeight()),
+			   (int)m_roi.ScaleX(pts[4],m_image.GetWidth()),(int)m_roi.ScaleY(pts[5],m_image.GetHeight()),
+			   (int)m_roi.ScaleX(pts[6],m_image.GetWidth()),(int)m_roi.ScaleY(pts[7],m_image.GetHeight()),
 			   0,1);
 			   
     return true;    
