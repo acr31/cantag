d6af3181ed063484678560841c65522eca185c92
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-10-06 15:02:34 +0000

    skeleton file for estimating the transform (e.g of a camera to world coordinates) based on a set of point correspondances
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@814 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ src/EstimateTransform.cc
@@ -0,0 +1,35 @@
+/*
+  Copyright (C) 2005 Andrew C. Rice
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Email: acr31@cam.ac.uk
+*/
+
+/**
+ * $Header$
+ */
+
+#include <cantag/EstimateTransform.hh>
+
+namespace Cantag {
+
+  Transform EstimateTransform::operator()(const Correspondances<LocationElement>& correspondances) {
+    for(std::list<std::pair<const TransformEntity,const LocationElement*> >::const_iterator i = correspondances.GetValues().begin();i!=correspondances.GetValues().end();++i) {
+      std::cout << (*i).second->x << " " << (*i).second->y << " " << (*i).second->z << std::endl;
+    }
+    return Transform();
+  };
+}
