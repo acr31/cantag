02df907087434d4eb463eb776f40c19016f5e5f1
rkh23 <rkh23@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2005-10-28 11:03:12 +0000

    Removed usage of atan2 function in ordering
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@890 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/algorithms/ConvexHull.cc
+++ src/algorithms/ConvexHull.cc
@@ -35,9 +35,7 @@ namespace Cantag {
     const std::vector<float>& V = source.GetPoints();
     std::vector<int>& H = dest.GetIndices();
     
-
-
-    // Find the point that has the greatest
+  // Find the point that has the greatest
     // x value. If there is a tie, take the
     // one with the highest y value
     int index=0;
@@ -57,38 +55,68 @@ namespace Cantag {
 
     // Sort the points by angle (0->2pi)
     // with this point as origin
-    std::map<float,int> imap;
+    // We use y/x to avoid inverse trig and this means
+    // we must carefully consider each quadrant
+
+    std::map<float,int> quadrants[2];
+    
     for (int i=0; i<V.size();i+=2) {
       if (i!=index) {
 	float x = V[i]-ix;
 	float y = V[i+1]-iy;
-	float ang=atan2(y,x);
-	if (ang<0.0) ang+=2*M_PI;
-	std::map<float,int>::const_iterator ci = imap.find(ang);
-	if (ci==imap.end()) imap[ang]=i/2;
+	
+	assert(x<=0.0);
+
+	int q=0;
+	if (x<=0 && y>0) q=0;
+	if (x<0 && y<=0) q=1;
+
+	float val=y/x;
+	if (x==0.0) val=1e10;
+	if(x==0 && y<0) val=-val;
+	std::map<float,int >::const_iterator ci = quadrants[q].find(val);
+	if (ci==quadrants[q].end()) {quadrants[q][val]=i/2;}
 	else {
 	  int idx = ci->second;
 	  float x1=V[i]-ix;
 	  float y1=V[i+1]-iy;
 	  float x2=V[2*idx]-ix;
 	  float y2=V[2*idx+1]-iy;
-	  if ((x1*x1+y1*y1) > (x2*x2+y2*y2)) imap[ang]=i/2;
+	  if ((x1*x1+y1*y1) > (x2*x2+y2*y2)) quadrants[q][val]=i/2;
 	}
       }
     }
 
     // Start the hull
+    std::map<float,int>::const_iterator ci;
+   
+    int q=0;
+    if (quadrants[0].size()) ci = quadrants[0].begin();
+    else {
+      if (!quadrants[1].size()) return false;
+      ci = quadrants[1].begin();
+      q=1;
+    }
+
     H.push_back(index/2);
-    std::map<float,int>::const_iterator ci = imap.begin();
     H.push_back(ci->second);
     ++ci;
 
-    // Walk through the map
+    // Walk through the maps
     // When we have to turn left add that vertex
     // When we have to turn right, remove the last vertex
-    for (;ci!=imap.end();++ci) {
+    for (;q!=2;++ci) {
+
+      // Walk through both quadrants
+      if (q==0 && ci==quadrants[0].end()) {
+	ci=quadrants[1].begin();
+	q=1;
+      }
+      else if (q==1 && ci==quadrants[1].end()) break;
+
       float px = V[ci->second*2];
       float py = V[ci->second*2+1];
+      
       float px_last = V[(H[H.size()-1])*2];
       float py_last = V[(H[H.size()-1])*2+1];
       float px_last2 = V[(H[H.size()-2])*2];
@@ -100,13 +128,12 @@ namespace Cantag {
       float t_x = (px-px_last);
       float t_y = (py-py_last);
 
+      // Cross product
       if ((s_x*t_y - s_y*t_x)<0) {
-	// Add this vertex
 	H.push_back(ci->second);
       }
       else {
 	float z=1;
-
 	while (z>0 && H.size()>1) {
 	  std::vector<int>::iterator dit = H.end();
 	  dit--;
@@ -147,6 +174,8 @@ namespace Cantag {
     }
 
     return true;
+
+   
 
   };
 }
