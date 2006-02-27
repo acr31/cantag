3bf110225fa5b04c4d4064d961ad584ccb73aaa5
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2006-02-27 22:41:19 +0000

    video for windows image source
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@1276 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ include/cantag/VFWImageSource.hh
@@ -0,0 +1,58 @@
+/*
+  Copyright (C) 2004 Andrew C. Rice
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Email: acr31@cam.ac.uk
+*/
+
+#ifndef VFWIMAGESOURCE_GUARD
+#define VFWIMAGESOURCE_GUARD
+
+#include <cantag/Config.hh>
+
+#ifndef WIN32
+# error "VFWImageSource requires Win32"
+#endif
+
+
+#include <cantag/Image.hh>
+#include <cantag/ImageSource.hh>
+
+namespace Cantag {
+	class VFWImageSource : public ImageSource<Pix::Sze::Byte1,Pix::Fmt::Grey8> {
+	public:
+		bool m_running;
+		static VFWImageSource* m_self;
+		void* m_captureWindow;
+		int m_width;
+		int m_height;
+		int m_bitcount;
+		Image<Pix::Sze::Byte1,Pix::Fmt::Grey8>* m_image;
+
+	public:
+		
+
+		int GetWidth() const { return m_width; }
+		int GetHeight() const { return m_height; }
+
+		VFWImageSource();
+		~VFWImageSource();
+		Image<Pix::Sze::Byte1,Pix::Fmt::Grey8>* Next();
+	};
+}
+
+
+#endif//VFWIMAGESOURCE_GUARD
