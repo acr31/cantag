87bd5d002445333033bfed2ce40e9e32ad0e601a
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2006-02-27 22:42:40 +0000

    added gsl includes
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@1279 02f9cdae-41fc-0310-9451-d5e721ae9d13

--- src/GlutRenderWindow.cc
+++ src/GlutRenderWindow.cc
@@ -26,6 +26,15 @@
 
 #ifdef HAVE_GL_FREEGLUT_H
 
+#ifdef WIN32
+# include <windows.h>
+#endif
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/freeglut.h>
+
+
 #include <cantag/GlutRenderWindow.hh>
 
 namespace Cantag {
