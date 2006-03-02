265d4c75665947cbfa74074525b5e6edc6aa9b6a
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2006-03-02 14:15:42 +0000

    separated out function for clearing the edge of the image in preparation for the contourfolloer
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@1297 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ src/algorithms/ContourFollowerClearImageBorder.cc
@@ -0,0 +1,40 @@
+/*
+  Copyright (C) 2006 Andrew C. Rice
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Email: acr31@cam.ac.uk
+*/
+
+#include <cantag/algorithms/ContourFollowerClearImageBorder.hh>
+
+namespace Cantag {
+
+bool ContourFollowerClearImageBorder::operator()(MonochromeImage& dest) const {
+	const int height = dest.GetHeight();
+	const int width = dest.GetWidth();
+	for(unsigned int i=0;i<height;++i) {
+      dest.SetPixel(0,i,false);
+      dest.SetPixel(width-1,i,false);
+      if (i == 0 || i == height-1) {
+		for(unsigned int j=0;j<width-1;++j) {
+		dest.SetPixel(j,i,false);
+		}
+      }
+	}
+	return true;
+}
+
+}
