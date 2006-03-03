419e6cc1efef960e351cdc1b073b59b21a28dabb
acr31 <acr31@02f9cdae-41fc-0310-9451-d5e721ae9d13>
2006-03-03 14:26:36 +0000

    kepress handling
    git-svn-id: file:///usr/groups/dtg/svn/Cantag/trunk@1310 02f9cdae-41fc-0310-9451-d5e721ae9d13

---
+++ include/cantag/Keypress.hh
@@ -0,0 +1,39 @@
+/*
+  Copyright (C) 2004 Andrew C. Rice
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+  Email: acr31@cam.ac.uk
+*/
+
+/**
+ * $Header$
+ */
+
+#ifndef KEYPRESS_GUARD
+#define KEYPRESS_GUARD
+
+#include <cantag/Config.hh>
+
+namespace Cantag {
+  namespace Key {
+    enum Code {
+      ESC,
+      A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+    };
+  }
+}
+
+#endif//KEYPRESS_GUARD
